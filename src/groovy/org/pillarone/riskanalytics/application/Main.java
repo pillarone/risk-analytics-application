c3a3e16073cc82dc06d3eec7e25eef0797b27e44
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-02-07 11:26:05 +0100

    added an extension for external databases

--- src/groovy/org/pillarone/riskanalytics/application/Main.java
+++ src/groovy/org/pillarone/riskanalytics/application/Main.java
@@ -8,15 +8,25 @@ import org.codehaus.groovy.grails.commons.BootstrapArtefactHandler;
 import org.codehaus.groovy.grails.commons.GrailsApplication;
 import org.codehaus.groovy.grails.commons.GrailsBootstrapClass;
 import org.codehaus.groovy.grails.commons.GrailsClass;
+import org.pillarone.riskanalytics.application.initialization.DatabaseManagingSessionStateListener;
+import org.pillarone.riskanalytics.application.initialization.IExternalDatabaseSupport;
 import org.pillarone.riskanalytics.application.initialization.StandaloneConfigLoader;
 import org.pillarone.riskanalytics.application.ui.P1RATStandaloneLauncher;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * Starting point of the standalone application.
+ * Initializes log4j and starts grails, then executes all BootStraps and starts the UI.
+ */
 public class Main {
 
     private static Log LOG = LogFactory.getLog(Main.class);
 
     public static void main(String args[]) {
+        launchApplication(null);
+    }
+
+    public static void launchApplication(IExternalDatabaseSupport databaseSupport) {
         try {
             String environment = System.getProperty("grails.env");
             if (environment == null) {
@@ -25,17 +35,20 @@ public class Main {
 
             }
             StandaloneConfigLoader.loadLog4JConfig(environment);
-
             LOG.info("Starting RiskAnalytics with environment " + environment);
-            ExpandoMetaClass.enableGlobally();
+
+            if(databaseSupport != null) {
+                LOG.info("Starting external database for environment " + environment);
+                databaseSupport.startDatabase();
+            }
+
 
             LOG.info("Loading grails..");
-
+            ExpandoMetaClass.enableGlobally();
             ApplicationContext ctx = GrailsUtil.bootstrapGrailsFromClassPath();
             GrailsApplication app = (GrailsApplication) ctx.getBean(GrailsApplication.APPLICATION_ID);
 
             LOG.info("Executing bootstraps..");
-
             GrailsClass[] bootstraps = app.getArtefacts(BootstrapArtefactHandler.TYPE);
             for (GrailsClass bootstrap : bootstraps) {
                 final GrailsBootstrapClass bootstrapClass = (GrailsBootstrapClass) bootstrap;
@@ -46,8 +59,11 @@ public class Main {
             }
 
             LOG.info("Loading user interface..");
-
-            P1RATStandaloneLauncher.start();
+            if (databaseSupport == null) {
+                P1RATStandaloneLauncher.start();
+            } else {
+                P1RATStandaloneLauncher.start(new DatabaseManagingSessionStateListener(databaseSupport));
+            }
         } catch (Exception e) {
             LOG.fatal("Startup failed", e);
         }
