04e56925637e52f76bf262c5c4b6191227ce6ec6
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-06-25 09:36:59 +0200

    fixed application version

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.0-RC-1.1"
+    def version = "1.0-RC-1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
