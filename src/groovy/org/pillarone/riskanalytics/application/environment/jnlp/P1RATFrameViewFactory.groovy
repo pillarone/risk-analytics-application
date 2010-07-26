147760e981be1c57186a88df10eaa04f7c769a56
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2010-07-26 17:04:36 +0200

    PMO-1048: Sandbox has wrong icon (Java instead of PillarOne)

--- src/groovy/org/pillarone/riskanalytics/application/environment/jnlp/P1RATFrameViewFactory.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/environment/jnlp/P1RATFrameViewFactory.groovy
@@ -3,8 +3,8 @@ package org.pillarone.riskanalytics.application.environment.jnlp
 import com.ulcjava.base.application.ULCFrame
 import com.ulcjava.base.application.ULCRootPane
 import com.ulcjava.base.application.util.Dimension
-import com.ulcjava.base.application.util.ULCIcon
 import org.pillarone.riskanalytics.application.environment.P1RATViewFactory
+import org.pillarone.riskanalytics.application.ui.util.UIUtils
 import org.pillarone.riskanalytics.application.ui.util.server.ULCMinimalSizeFrame
 
 class P1RATFrameViewFactory extends P1RATViewFactory {
@@ -12,10 +12,7 @@ class P1RATFrameViewFactory extends P1RATViewFactory {
 
     ULCRootPane createRootPane() {
         ULCMinimalSizeFrame frame = new ULCMinimalSizeFrame()
-        URL resource = getClass().getResource("/application.png")
-        if (resource) {
-            frame.setIconImage(new ULCIcon(resource))
-        }
+        frame.setIconImage(UIUtils.getIcon("application.png"))
         frame.title = "Risk Analytics"
 
         frame.setDefaultCloseOperation(ULCFrame.TERMINATE_ON_CLOSE)
