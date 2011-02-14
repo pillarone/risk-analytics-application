271cbdc09de2c4bf2d1defed423d658e51af551f
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-02-14 19:04:43 +0100

    PMO-1440: Accessibility of detailed result viewer

--- test/unit/org/pillarone/riskanalytics/application/ui/result/view/SingleCollectorViewTests.groovy
+++ test/unit/org/pillarone/riskanalytics/application/ui/result/view/SingleCollectorViewTests.groovy
@@ -1,6 +1,8 @@
 package org.pillarone.riskanalytics.application.ui.result.view
 
 import com.ulcjava.base.application.ULCComponent
+import com.ulcjava.testframework.operator.ULCButtonOperator
+import com.ulcjava.testframework.operator.ULCTextFieldOperator
 import org.pillarone.riskanalytics.application.ui.AbstractP1RATTestCase
 import org.pillarone.riskanalytics.application.ui.result.model.ResultTableTreeNode
 import org.pillarone.riskanalytics.application.ui.result.model.SingleValueCollectorTableTreeModel
@@ -12,10 +14,50 @@ import org.pillarone.riskanalytics.application.ui.result.model.SingleValueTreeBu
 class SingleCollectorViewTests extends AbstractP1RATTestCase {
 
     SingleValueCollectorTableTreeModel singleValueCollectorTableTreeModel
+    int maxIteration = 201
 
     public void testView() {
-        Thread.sleep 1000
+//        Thread.sleep 1000
+        ULCButtonOperator previousButton = getButtonOperator("SingleCollectorView.previousButton")
+        ULCButtonOperator nextButton = getButtonOperator("SingleCollectorView.nextButton")
+        assertFalse previousButton.isEnabled()
+        assertTrue nextButton.isEnabled()
+
+        ULCTextFieldOperator from = getTextFieldOperator("SingleCollectorView.fromTextField")
+        ULCTextFieldOperator to = getTextFieldOperator("SingleCollectorView.toTextField")
+        to.getFocus()
+        to.clearText()
+        // 300 greater than simulationRun.iterations 201
+        to.typeText("300")
+        ULCButtonOperator applyButton = getButtonOperator("SingleCollectorView.apply")
+
+
+        applyButton.getFocus()
+        applyButton.clickMouse()
+
+
+        assertFalse previousButton.isEnabled()
+        assertFalse nextButton.isEnabled()
+
+        assertEquals String.valueOf(maxIteration), to.getText()
+
+        from.getFocus()
+        from.clearText()
+        from.typeText("100")
+        applyButton.getFocus()
+        applyButton.clickMouse()
+
+        assertTrue previousButton.isEnabled()
+        assertFalse nextButton.isEnabled()
+
+        previousButton.getFocus()
+        previousButton.clickMouse()
+
+        assertTrue nextButton.isEnabled()
+
     }
+
+
 
     ULCComponent createContentPane() {
         singleValueCollectorTableTreeModel = getMockModel()
@@ -31,11 +73,11 @@ class SingleCollectorViewTests extends AbstractP1RATTestCase {
         model.iterations = 1
         model.periodCount = 1
         model.nodes = [new ResultTableTreeNode("node1"), new ResultTableTreeNode("node2")]
-        model.metaClass.setIterations = {->
-            model.iterations = 1
+        model.metaClass.getMaxIteration = {->
+            return maxIteration
         }
         model.metaClass.init = {->
-            model.builder = new SingleValueTreeBuilder(model.singleValueResultsMap, model.iterations, 2, 1)
+            model.builder = new SingleValueTreeBuilder(model.singleValueResultsMap, 1, model.iterations, 2, 1)
             model.builder.build()
         }
 
