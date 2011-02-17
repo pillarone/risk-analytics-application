43cb007cf99e5d0b2901ad1eb67c2f269c28f760
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-02-17 17:19:25 +0100

    ART-329: Default result template

--- src/groovy/org/pillarone/riskanalytics/application/ui/simulation/model/ResultConfigurationNameListModel.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/simulation/model/ResultConfigurationNameListModel.groovy
@@ -1,11 +1,18 @@
 package org.pillarone.riskanalytics.application.ui.simulation.model
 
-import com.ulcjava.base.application.DefaultComboBoxModel
 import org.pillarone.riskanalytics.core.output.ResultConfigurationDAO
+
+import com.ulcjava.base.application.DefaultComboBoxModel
+import org.pillarone.riskanalytics.application.util.UserPreferences
 
 public class ResultConfigurationNameListModel extends DefaultComboBoxModel {
 
     protected Class modelClass
+    UserPreferences userPreferences
+
+    public ResultConfigurationNameListModel() {
+        userPreferences = new UserPreferences()
+    }
 
     void load(Class modelClass) {
         if (this.modelClass != modelClass) {
@@ -27,11 +34,26 @@ public class ResultConfigurationNameListModel extends DefaultComboBoxModel {
                 distinct("name")
             }
         }
-
+        Object defaultItem = getDefaultItem(modelClass.name)
         params.each {
             addElement(it)
         }
-        selectedItem = getElementAt(0)
+        selectedItem = defaultItem ? defaultItem : getElementAt(0)
     }
+
+    @Override
+    void setSelectedItem(Object o) {
+        userPreferences.setDefaultResult(modelClass.name, o)
+        super.setSelectedItem(o)
+    }
+
+    Object getDefaultItem(String modelClassName) {
+        return userPreferences.getDefaultResult(modelClassName)
+    }
+
+    void setDefaultItem(String modelClassName, String defaultResult) {
+        userPreferences.setDefaultResult(modelClassName, defaultResult)
+    }
+
 
 }
