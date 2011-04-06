d41cede1172f741719e52d65d6397ec7ed98d02f
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-04-06 18:41:01 +0200

    releasing new plugin version 1.4-ALPHA-1.2

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.application.example.constraint.CopyPasteConst
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.4-ALPHA-1.1"
+    def version = "1.4-ALPHA-1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
