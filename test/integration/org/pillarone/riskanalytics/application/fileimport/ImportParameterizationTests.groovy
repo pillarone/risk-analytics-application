86789d5524bb241b96b7eebc4fad0064a8788016
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-04-29 15:36:01 +0200

    fixe a tests

--- test/integration/org/pillarone/riskanalytics/application/fileimport/ImportParameterizationTests.groovy
+++ test/integration/org/pillarone/riskanalytics/application/fileimport/ImportParameterizationTests.groovy
@@ -2,22 +2,33 @@ package org.pillarone.riskanalytics.application.fileimport
 
 import models.application.ApplicationModel
 import org.pillarone.riskanalytics.application.output.structure.ResultStructureDAO
-import org.pillarone.riskanalytics.application.output.structure.StructureMapping
 import org.pillarone.riskanalytics.application.output.structure.item.ResultNode
 import org.pillarone.riskanalytics.application.output.structure.item.ResultStructure
+import org.pillarone.riskanalytics.core.output.DBCleanUpService
 
-class ResultStructureImportServiceTests extends GroovyTestCase {
+/**
+ * @author fouad.jaada@intuitive-collaboration.com
+ */
+class ImportParameterizationTests extends GroovyTestCase {
 
     ResultStructureImportService service = new ResultStructureImportService()
 
-    void testImport() {
-        int initialMappings = StructureMapping.count()
+    @Override
+    protected void setUp() {
+        super.setUp()
+        new DBCleanUpService().cleanUp()
+    }
 
-        ResultStructureImportService.importDefaults()
 
-        List allDaos = ResultStructureDAO.list()
-        assertEquals allDaos.collect { it.modelClassName + " " + it.name}.toString(), 5, allDaos.size()
-        assertTrue StructureMapping.count() > initialMappings
+
+    void testImportParameterization() {
+
+        File paramFile = new File(getModelFolder(), "application/ApplicationDefaultResultTree.groovy")
+
+        def count = ResultStructureDAO.count()
+
+        assertTrue "import not successful", service.importFile(paramFile.toURI().toURL())
+        assertEquals count + 1, ResultStructureDAO.count()
 
         ResultStructure resultStructure = new ResultStructure(ResultStructureImportService.DEFAULT_NAME, ApplicationModel)
         resultStructure.load()
@@ -25,35 +36,38 @@ class ResultStructureImportServiceTests extends GroovyTestCase {
         ResultNode node = resultStructure.rootNode
         assertEquals 2, node.childCount
 
-        ResultNode dC = node.getChildByName("dynamicComponent")
+        ResultNode dC = node.getChildAt(0)
+        assertEquals "dynamicComponent", dC.name
         assertEquals 2, dC.childCount
 
-        ResultNode subComponents = dC.getChildByName("[%subcomponents%]")
-        assertNotNull subComponents
+        ResultNode subComponents = dC.getChildAt(0)
+        assertEquals "[%subcomponents%]", subComponents.name
         assertEquals 2, subComponents.childCount
 
-        ResultNode outSecondValue = subComponents.getChildByName("outSecondValue")
-        assertNotNull outSecondValue
+        ResultNode outSecondValue = subComponents.getChildAt(0)
+        assertEquals "outSecondValue", outSecondValue.name
         assertEquals 1, outSecondValue.childCount
 
-        ResultNode xValue = outSecondValue.getChildByName("value")
-        assertNotNull xValue
+        ResultNode xValue = outSecondValue.getChildAt(0)
+        assertEquals "value", xValue.name
         assertEquals "Application:dynamicComponent:[%subcomponents%]:outSecondValue:value", xValue.resultPath
         assertEquals 0, xValue.childCount
 
-        ResultNode outValue1 = dC.getChildByName("outValue1")
-        assertNotNull outValue1
+        ResultNode outValue1 = dC.getChildAt(1)
+        assertEquals "outValue1", outValue1.name
         assertEquals 1, outValue1.childCount
 
-        ResultNode value = outValue1.getChildByName("value")
-        assertNotNull value
+        ResultNode value = outValue1.getChildAt(0)
+        assertEquals "value", value.name
         assertEquals "Application:dynamicComponent:outValue1:value", value.resultPath
         assertEquals 0, value.childCount
+
+
     }
-
-
 
     private File getModelFolder() {
         return new File(getClass().getResource("/models").toURI())
     }
+
+
 }
