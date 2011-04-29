48dc50c33b856090035f5b4a5009b0c37e1d524e
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-04-29 14:57:08 +0200

    fixe a tests

--- test/integration/org/pillarone/riskanalytics/application/fileimport/ResultStructureImportServiceTests.groovy
+++ test/integration/org/pillarone/riskanalytics/application/fileimport/ResultStructureImportServiceTests.groovy
@@ -1,11 +1,10 @@
 package org.pillarone.riskanalytics.application.fileimport
 
-import org.pillarone.riskanalytics.application.output.structure.StructureMapping
-import org.pillarone.riskanalytics.application.output.structure.ResultStructureDAO
-import org.pillarone.riskanalytics.application.output.structure.item.ResultStructure
-import org.pillarone.riskanalytics.application.output.structure.item.ResultNode
 import models.application.ApplicationModel
-
+import org.pillarone.riskanalytics.application.output.structure.ResultStructureDAO
+import org.pillarone.riskanalytics.application.output.structure.StructureMapping
+import org.pillarone.riskanalytics.application.output.structure.item.ResultNode
+import org.pillarone.riskanalytics.application.output.structure.item.ResultStructure
 
 class ResultStructureImportServiceTests extends GroovyTestCase {
 
@@ -26,29 +25,28 @@ class ResultStructureImportServiceTests extends GroovyTestCase {
         ResultNode node = resultStructure.rootNode
         assertEquals 2, node.childCount
 
-        ResultNode dC = node.getChildAt(1)
-        assertEquals "dynamicComponent", dC.name
+        ResultNode dC = node.getChildByName("dynamicComponent")
         assertEquals 2, dC.childCount
 
-        ResultNode subComponents = dC.getChildAt(1)
-        assertEquals "[%subcomponents%]", subComponents.name
+        ResultNode subComponents = dC.getChildByName("[%subcomponents%]")
+        assertNotNull subComponents
         assertEquals 2, subComponents.childCount
 
-        ResultNode outSecondValue = subComponents.getChildAt(1)
-        assertEquals "outSecondValue", outSecondValue.name
+        ResultNode outSecondValue = subComponents.getChildByName("outSecondValue")
+        assertNotNull outSecondValue
         assertEquals 1, outSecondValue.childCount
 
-        ResultNode xValue = outSecondValue.getChildAt(0)
-        assertEquals "value", xValue.name
+        ResultNode xValue = outSecondValue.getChildByName("value")
+        assertNotNull xValue
         assertEquals "Application:dynamicComponent:[%subcomponents%]:outSecondValue:value", xValue.resultPath
         assertEquals 0, xValue.childCount
 
-        ResultNode outValue1 = dC.getChildAt(0)
-        assertEquals "outValue1", outValue1.name
+        ResultNode outValue1 = dC.getChildByName("outValue1")
+        assertNotNull outValue1
         assertEquals 1, outValue1.childCount
 
-        ResultNode value = outValue1.getChildAt(0)
-        assertEquals "value", value.name
+        ResultNode value = outValue1.getChildByName("value")
+        assertNotNull value
         assertEquals "Application:dynamicComponent:outValue1:value", value.resultPath
         assertEquals 0, value.childCount
     }
