f8c8fb98081c57330001dec81d840033a0ebe40d
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-07-22 19:31:18 +0200

    new plugin

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.4-ALPHA-6.2"
+    def version = "1.4-ALPHA-6.2.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
