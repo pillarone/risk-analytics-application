67e0378059310b1b3135dc2ec9237417c8642106
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-09-09 14:32:32 +0200

    PMO-1819: fixed tree node copy order

---
+++ src/groovy/org/pillarone/riskanalytics/application/ui/base/action/TreeNodeComparator.groovy
@@ -0,0 +1,30 @@
+package org.pillarone.riskanalytics.application.ui.base.action
+
+import com.ulcjava.base.application.tabletree.DefaultTableTreeModel
+import com.ulcjava.base.application.tabletree.ITableTreeNode
+
+class TreeNodeComparator implements Comparator<ITableTreeNode> {
+
+    private ITableTreeNode root
+
+    TreeNodeComparator(ITableTreeNode root) {
+        this.root = root
+    }
+
+    int compare(ITableTreeNode node1, ITableTreeNode node2) {
+        ITableTreeNode[] path1 = DefaultTableTreeModel.getPathToRoot(node1)
+        ITableTreeNode[] path2 = DefaultTableTreeModel.getPathToRoot(node2)
+
+        ITableTreeNode commonParent = path1[0]
+        int foundIndex = 0
+        for (int i = 1; i < path1.length && i < path2.length; i++) {
+            if (path1[i] == path2[i]) {
+                commonParent = path1[i]
+                foundIndex = i
+            }
+        }
+
+        return commonParent.getIndex(path1[foundIndex + 1]) < commonParent.getIndex(path2[foundIndex + 1]) ? -1 : 1
+    }
+
+}
