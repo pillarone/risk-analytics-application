a5f8cbbf88c973700d226ad95fed226b5d4ff7ed
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-10-01 21:25:07 +0200

    release new plugin version 1.4-BETA-8.2 fixing renamings

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.4-BETA-8.1"
+    def version = "1.4-BETA-8.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
