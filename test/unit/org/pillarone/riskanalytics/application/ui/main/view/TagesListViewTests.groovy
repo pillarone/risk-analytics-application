21b9cec7ed38c9c191b864ad46ae9ffc08a7dee4
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-10-01 20:18:36 +0200

    release new plugin version 1.4-BETA-8.1

--- test/unit/org/pillarone/riskanalytics/application/ui/main/view/TagesListViewTests.groovy
+++ test/unit/org/pillarone/riskanalytics/application/ui/main/view/TagesListViewTests.groovy
@@ -5,12 +5,11 @@ import com.ulcjava.base.application.ULCComponent
 import org.pillarone.riskanalytics.core.simulation.item.Parameterization
 import org.pillarone.riskanalytics.core.parameter.comment.Tag
 import com.ulcjava.testframework.operator.ULCCheckBoxOperator
-import com.ulcjava.base.application.util.Color
 
 /**
  * @author fouad.jaada@intuitive-collaboration.com
  */
-class TagesListViewTests extends AbstractP1RATTestCase {
+class TagsListViewTests extends AbstractP1RATTestCase {
     Parameterization p1 = new Parameterization("p1")
     Parameterization p2 = new Parameterization("p2")
 
@@ -19,7 +18,7 @@ class TagesListViewTests extends AbstractP1RATTestCase {
     Tag tag3 = new Tag(name: "tag3")
 
     public void testView() {
-//        Thread.sleep(1000)
+        Thread.sleep(1000)
         ULCCheckBoxOperator box1 = getCheckBoxOperator("tag1")
         assertNotNull(box1)
         assertTrue p1.getTags().contains(tag1)
@@ -52,7 +51,7 @@ class TagesListViewTests extends AbstractP1RATTestCase {
         p1.setTags([tag1, tag2] as Set)
         p2.setTags([tag1] as Set)
 
-        TagesListView tagesListView = new TagesListView([p1, p2])
+        TagsListView tagesListView = new TagsListView([p1, p2])
         tagesListView.metaClass.getAllTags = {-> [tag1, tag2, tag3]}
         tagesListView.metaClass.getAllModellingItemTages = {-> [tag1, tag2]}
 
