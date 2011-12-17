ea4e17b5e2a9470dbcf7aad8c97d2450298379cc
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-12-17 20:20:23 +0100

    releasing new plugin version 1.4-RC-3

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.4-RC-2.3"
+    def version = "1.4-RC-3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
