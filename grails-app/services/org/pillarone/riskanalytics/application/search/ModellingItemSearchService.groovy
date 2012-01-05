926dc08e47e2e34247143c2586ba5b675883d55f
bzetterstrom <bjorn.zetterstrom@art-allianz.com>
2012-01-05 17:29:05 +0100

    Fixed issue with simulations being marked for deletions being picked up by the ModellingItemSearchService

--- grails-app/services/org/pillarone/riskanalytics/application/search/ModellingItemSearchService.groovy
+++ grails-app/services/org/pillarone/riskanalytics/application/search/ModellingItemSearchService.groovy
@@ -87,7 +87,7 @@ class ModellingItemSearchService {
             for (ResultConfigurationDAO dao in ResultConfigurationDAO.list()) {
                 indexWriter.addDocument(org.pillarone.riskanalytics.application.search.DocumentFactory.createDocument(toResultConfiguration(dao)))
             }
-            for (SimulationRun dao in SimulationRun.list()) {
+            for (SimulationRun dao in SimulationRun.list().findAll { ! it.toBeDeleted }) {
                 indexWriter.addDocument(org.pillarone.riskanalytics.application.search.DocumentFactory.createDocument(toSimulation(dao)))
             }
         } finally {
