0e509b644d534385b1bdef9e541c79509d0e5b1f
bzetterstrom <bjorn.zetterstrom@art-allianz.com>
2012-02-22 16:04:14 +0100

    added missing class

---
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/action/SetFilterToSelection.groovy
@@ -0,0 +1,36 @@
+package org.pillarone.riskanalytics.application.ui.main.action
+
+import com.ulcjava.base.application.ULCTableTree
+import com.ulcjava.base.application.event.ActionEvent
+import org.pillarone.riskanalytics.application.ui.parameterization.model.ParameterizationNode
+import org.pillarone.riskanalytics.application.ui.main.view.NavigationBarTopPane
+import org.pillarone.riskanalytics.application.ui.main.view.RiskAnalyticsMainModel
+import com.canoo.ulc.community.ulcclipboard.server.ULCClipboard
+import org.pillarone.riskanalytics.core.simulation.item.Parameterization
+
+/**
+ * User: bzetterstrom
+ * Date: 1/5/12
+ */
+class SetFilterToSelection extends SelectionTreeAction {
+    SetFilterToSelection(ULCTableTree tree, RiskAnalyticsMainModel model) {
+        super("SetFilterToSelection", tree, model)
+    }
+
+    @Override
+    void doActionPerformed(ActionEvent event) {
+        List<ParameterizationNode> elements = getSelectedObjects(Parameterization.class)
+        final StringBuilder sb = new StringBuilder();
+        for (ParameterizationNode node : elements) {
+            if (sb.length() > 0) {
+                sb.append(" OR ")
+            }
+            sb.append("\"")
+            sb.append(node.getAbstractUIItem().getNameAndVersion())
+            sb.append("\"")
+        }
+        ULCClipboard.getClipboard().content = sb.toString()
+    }
+
+
+}
