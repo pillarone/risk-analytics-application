ec77fcd1f88a3cfdac76701822fdb0232743abd9
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-03-01 08:35:46 +0100

    PMO-1970: [%period%] enabled for result trees

--- test/unit/org/pillarone/riskanalytics/application/output/structure/ResultStructureTreeBuilderTests.groovy
+++ test/unit/org/pillarone/riskanalytics/application/output/structure/ResultStructureTreeBuilderTests.groovy
@@ -3,19 +3,33 @@ package org.pillarone.riskanalytics.application.output.structure
 import org.pillarone.riskanalytics.application.fileimport.TreeBuildingClosureDelegate
 import org.pillarone.riskanalytics.application.output.structure.item.ResultStructure
 import org.pillarone.riskanalytics.application.ui.base.model.SimpleTableTreeNode
-import org.pillarone.riskanalytics.application.util.LocaleResources
 import org.pillarone.riskanalytics.core.output.AggregatedCollectingModeStrategy
 import org.pillarone.riskanalytics.core.output.CollectingModeFactory
 import org.pillarone.riskanalytics.core.output.ICollectingModeStrategy
 import org.pillarone.riskanalytics.core.output.SingleValueCollectingModeStrategy
+import org.pillarone.riskanalytics.core.simulation.item.Simulation
+import org.joda.time.DateTime
+import org.pillarone.riskanalytics.core.simulation.item.Parameterization
+import models.application.ApplicationModel
+import org.pillarone.riskanalytics.core.model.Model
 
 class ResultStructureTreeBuilderTests extends GroovyTestCase {
 
+    Simulation simulation
+    Model model
+    
     protected void setUp() {
         super.setUp();
 
         CollectingModeFactory.registerStrategy(new SingleValueCollectingModeStrategy())
         CollectingModeFactory.registerStrategy(new AggregatedCollectingModeStrategy())
+        simulation = new Simulation('sim')
+        simulation.parameterization = new Parameterization('p14n')
+        simulation.parameterization.periodLabels = ['2012-01-01','2013-01-01','2013-01-01']
+        simulation.periodCount = 3
+        simulation.start = new DateTime(2012,1,1,0,0,0,0)
+        simulation.modelClass = ApplicationModel.class
+        model = new ApplicationModel()
     }
 
     void testOneWildcard() {
@@ -69,7 +83,7 @@ class ResultStructureTreeBuilderTests extends GroovyTestCase {
         ResultStructure resultStructure = new ResultStructure("name")
         resultStructure.rootNode = TreeBuildingClosureDelegate.createStructureTree(mapping)
 
-        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, null, resultStructure, null)
+        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, model, resultStructure, simulation)
 
         SimpleTableTreeNode root = treeBuilder.buildTree()
         assertEquals "model", root.getName()
@@ -144,7 +158,7 @@ class ResultStructureTreeBuilderTests extends GroovyTestCase {
         ResultStructure resultStructure = new ResultStructure("name")
         resultStructure.rootNode = TreeBuildingClosureDelegate.createStructureTree(mapping)
 
-        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, null, resultStructure, null)
+        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, model, resultStructure, simulation)
 
         SimpleTableTreeNode root = treeBuilder.buildTree()
         assertEquals "model", root.getName()
@@ -189,7 +203,7 @@ class ResultStructureTreeBuilderTests extends GroovyTestCase {
         ResultStructure resultStructure = new ResultStructure("name")
         resultStructure.rootNode = TreeBuildingClosureDelegate.createStructureTree(mapping)
 
-        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, null, resultStructure, null)
+        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, model, resultStructure, simulation)
 
         SimpleTableTreeNode root = treeBuilder.buildTree()
         assertEquals "model", root.getName()
@@ -226,5 +240,79 @@ class ResultStructureTreeBuilderTests extends GroovyTestCase {
 
         assertTrue new ResultStructureTreeBuilder.NodeReplacement(path: path, wildcard: "[%claimsGenerator%]").compareTo(new ResultStructureTreeBuilder.NodeReplacement(path: path2, wildcard: "[%lineOfBusiness%]")) < 0
         assertTrue new ResultStructureTreeBuilder.NodeReplacement(path: path2, wildcard: "[%contract%]").compareTo(new ResultStructureTreeBuilder.NodeReplacement(path: path, wildcard: "[%lineOfBusiness%]")) < 0
+    }
+    
+    void testReplacementPeriod() {
+        //multiple result path per level (test wildcard replacing - should only happen once per level)
+        //mapping larger than allPaths (tests if unnecessary nodes are removed)
+        //non wildcard-paths
+        //out on DCC level (incl order)
+        //collector injection
+        Closure mapping = {
+            model {
+                net {
+                    totalFigureX "Model:linesOfBusiness:outTotalNet:figureX", {
+                        "[%period%]" "Model:linesOfBusiness:outTotalNet:[%period%]:figureX"
+                    }
+                    "[%lob%]" {
+                        premium "Model:linesOfBusiness:[%lob%]:outUnderwritingNet:premium", {
+                            "[%period%]" "Model:linesOfBusiness:[%lob%]:outUnderwritingNet:[%period%]:premium"
+                        }
+                    }
+                }
+            }
+        }
+        ICollectingModeStrategy aggregated = CollectingModeFactory.getStrategy(AggregatedCollectingModeStrategy.IDENTIFIER)
+        ICollectingModeStrategy single = CollectingModeFactory.getStrategy(SingleValueCollectingModeStrategy.IDENTIFIER)
+
+        Map allPaths = [
+                "Model:linesOfBusiness:subFire:outClaimsNet:ultimate": aggregated,
+                "Model:linesOfBusiness:subFire:outClaimsGross:ultimate": aggregated,
+                "Model:linesOfBusiness:subFire:outUnderwritingNet:premium": aggregated,
+                "Model:linesOfBusiness:subFire:outUnderwritingGross:premium": aggregated,
+
+                "Model:linesOfBusiness:outTotalNet:figureX": single,
+                "Model:linesOfBusiness:outTotalNet:figureY": single,
+
+                "Model:linesOfBusiness:subFlood:outClaimsNet:ultimate": aggregated,
+                "Model:linesOfBusiness:subFlood:outClaimsGross:ultimate": aggregated,
+                "Model:linesOfBusiness:subFlood:outUnderwritingNet:premium": aggregated,
+                "Model:linesOfBusiness:subFlood:outUnderwritingGross:premium": aggregated,
+        ]
+
+        ResultStructure resultStructure = new ResultStructure("name")
+        resultStructure.rootNode = TreeBuildingClosureDelegate.createStructureTree(mapping)
+
+        ResultStructureTreeBuilder treeBuilder = new ResultStructureTreeBuilder(allPaths, model, resultStructure, simulation)
+
+        SimpleTableTreeNode root = treeBuilder.buildTree()
+        assertEquals "model", root.getName()
+
+        assertEquals 1, root.childCount //ceded not collected
+
+        SimpleTableTreeNode net = root.getChildAt(0)
+        assertEquals "net", net.name
+        assertEquals 3, net.childCount //figureX + 2 dynamic subcomponents + figureY
+
+        assertEquals "totalFigureX", net.getChildAt(0).name
+        assertEquals "Model:linesOfBusiness:outTotalNet", net.getChildAt(0).path
+        assertEquals "figureX", net.getChildAt(0).field
+        assertEquals single.identifier, net.getChildAt(0).collector
+
+        SimpleTableTreeNode dynamicNode = net.getChildAt(1)
+        assertEquals 1, dynamicNode.childCount //claims + premium
+
+        assertEquals "premium", dynamicNode.getChildAt(0).name
+        assertEquals "Model:linesOfBusiness:subFire:outUnderwritingNet", dynamicNode.getChildAt(0).path
+        assertEquals "premium", dynamicNode.getChildAt(0).field
+        assertEquals aggregated.identifier, dynamicNode.getChildAt(0).collector
+
+        dynamicNode = net.getChildAt(2)
+        assertEquals 1, dynamicNode.childCount //claims + premium
+
+        assertEquals "premium", dynamicNode.getChildAt(0).name
+        assertEquals "Model:linesOfBusiness:subFlood:outUnderwritingNet", dynamicNode.getChildAt(0).path
+        assertEquals "premium", dynamicNode.getChildAt(0).field
+        assertEquals aggregated.identifier, dynamicNode.getChildAt(0).collector
     }
 }
