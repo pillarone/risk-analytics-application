4d76d0f10f08466789102505ac37f68ecfbd09f6
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2012-07-31 16:06:16 +0200

    fix merge error

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 import org.apache.ivy.plugins.resolver.FileSystemResolver
 
 //Use a custom plugins dir, because different branches use different plugin versions
-//grails.project.plugins.dir = "../local-plugins/RiskAnalyticsApplication-kti"
+grails.project.plugins.dir = "../local-plugins/RiskAnalyticsApplication-kti"
 
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
