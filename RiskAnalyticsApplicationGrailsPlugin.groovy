51196645648239d797c68b9b5da23adde00f9a63
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2013-03-19 11:34:26 +0100

    merged from kti

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import org.pillarone.riskanalytics.application.ULCAwareHibernateListener
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.6-RC-13-kti"
+    def version = "1.7-a1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
