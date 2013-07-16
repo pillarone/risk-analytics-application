8169891b5ba64fa592c7f50c1060eb6210a263f4
detlef-brendle <detlef.brendle@canoo.com>
2013-07-16 13:54:50 +0200

    cleanup

--- src/groovy/org/pillarone/riskanalytics/application/ui/base/model/modellingitem/ModellingInformationTableTreeModel.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/base/model/modellingitem/ModellingInformationTableTreeModel.groovy
@@ -153,7 +153,6 @@ class ModellingInformationTableTreeModel extends AbstractTableTreeModel {
                     builder.itemChanged(itemEvent.item)
             }
         }
-
     }
 
     public void refreshBatchNode() {
