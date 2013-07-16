3a60b66b654176c484376a631a0073bc58ea869c
detlef-brendle <detlef.brendle@canoo.com>
2013-07-16 13:12:27 +0200

    PMO-2550 : map more fields to index to reduce reloading items when searching

--- src/groovy/org/pillarone/riskanalytics/application/search/DocumentFactory.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/search/DocumentFactory.groovy
@@ -4,18 +4,29 @@ import org.apache.lucene.document.Document
 import org.apache.lucene.document.Field
 import org.apache.lucene.document.Field.Index
 import org.apache.lucene.document.Field.Store
+import org.joda.time.DateTime
+import org.pillarone.riskanalytics.core.parameter.comment.Tag
 import org.pillarone.riskanalytics.core.simulation.item.ModellingItem
 import org.pillarone.riskanalytics.core.simulation.item.Parameterization
 import org.pillarone.riskanalytics.core.simulation.item.Resource
 import org.pillarone.riskanalytics.core.simulation.item.ResultConfiguration
 import org.pillarone.riskanalytics.core.simulation.item.Simulation
 import org.pillarone.riskanalytics.core.simulation.item.VersionNumber
+import org.pillarone.riskanalytics.core.user.Person
 import org.pillarone.riskanalytics.core.util.GroovyUtils
+import org.pillarone.riskanalytics.core.workflow.Status
 
 abstract class DocumentFactory {
 
     public static final String NAME_FIELD = "name"
     public static final String VERSION_FIELD = "version"
+    public static final String STATE_FIELD = "state"
+    public static final String DEALID_FIELD = "dealId"
+    public static final String VALID_FIELD = "valid"
+    public static final String OWNER_FIELD = "owner"
+    public static final String LASTUPDATED_BY = "lastupdatedBy"
+    public static final String CREATION_TIME = "created"
+    public static final String LASTUPDATED_TIME = "updated"
     public static final String MODEL_CLASS_FIELD = "modelClass"
     public static final String ITEM_TYPE_FIELD = "itemType"
     public static final String TAGS_FIELD = "tags"
@@ -45,38 +56,68 @@ abstract class DocumentFactory {
         document.add(new Field(VERSION_FIELD, item.versionNumber.toString(), Store.YES, Index.ANALYZED))
         document.add(new Field(MODEL_CLASS_FIELD, item.modelClass.name, Store.YES, Index.ANALYZED))
         document.add(new Field(ITEM_TYPE_FIELD, ItemType.RESOURCE.toString(), Store.YES, Index.ANALYZED))
-        document.add(new Field(TAGS_FIELD, item.tags*.name.join(" "), Store.NO, Index.ANALYZED))
+        document.add(new Field(STATE_FIELD, item.status.toString(), Store.YES, Index.ANALYZED))
+        document.add(new Field(TAGS_FIELD, item.tags*.name.join(" "), Store.YES, Index.ANALYZED))
+        document.add(new Field(VALID_FIELD, item.valid.toString(), Store.YES, Index.NOT_ANALYZED))
+        addFieldIfNotNull(document, OWNER_FIELD, item.creator?.username?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_BY, item.lastUpdater?.username?.toString())
+        addFieldIfNotNull(document, CREATION_TIME, item.creationDate?.millis?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_TIME, item.modificationDate?.millis?.toString())
+
 
         return document
     }
 
-    private static Document doCreateDocument(Parameterization parameterization) {
+    private static Document doCreateDocument(Parameterization item) {
         Document document = new Document()
-        document.add(new Field(NAME_FIELD, parameterization.name, Store.YES, Index.ANALYZED))
-        document.add(new Field(VERSION_FIELD, parameterization.versionNumber.toString(), Store.YES, Index.ANALYZED))
-        document.add(new Field(MODEL_CLASS_FIELD, parameterization.modelClass.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(NAME_FIELD, item.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(VERSION_FIELD, item.versionNumber.toString(), Store.YES, Index.ANALYZED))
+        document.add(new Field(MODEL_CLASS_FIELD, item.modelClass.name, Store.YES, Index.ANALYZED))
         document.add(new Field(ITEM_TYPE_FIELD, ItemType.PARAMETERIZATION.toString(), Store.YES, Index.ANALYZED))
-        document.add(new Field(TAGS_FIELD, parameterization.tags*.name.join(" "), Store.NO, Index.ANALYZED))
+        document.add(new Field(TAGS_FIELD, item.tags*.name.join(" "), Store.YES, Index.ANALYZED))
+        document.add(new Field(STATE_FIELD, item.status.toString(), Store.YES, Index.ANALYZED))
+        document.add(new Field(VALID_FIELD, item.valid.toString(), Store.YES, Index.NOT_ANALYZED))
+        addFieldIfNotNull(document, DEALID_FIELD, item.dealId?.toString())
+        addFieldIfNotNull(document, OWNER_FIELD, item.creator?.username?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_BY, item.lastUpdater?.username?.toString())
+        addFieldIfNotNull(document, CREATION_TIME, item.creationDate?.millis?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_TIME, item.modificationDate?.millis?.toString())
+
 
         return document
     }
 
-    private static Document doCreateDocument(ResultConfiguration resultConfiguration) {
+    private static void addFieldIfNotNull(Document document, String fieldName, String fieldValue) {
+        if (fieldValue) {
+            document.add(new Field(fieldName, fieldValue, Store.YES, Index.NOT_ANALYZED))
+        }
+
+    }
+
+    private static Document doCreateDocument(ResultConfiguration item) {
         Document document = new Document()
-        document.add(new Field(NAME_FIELD, resultConfiguration.name, Store.YES, Index.ANALYZED))
-        document.add(new Field(VERSION_FIELD, resultConfiguration.versionNumber.toString(), Store.YES, Index.ANALYZED))
-        document.add(new Field(MODEL_CLASS_FIELD, resultConfiguration.modelClass.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(NAME_FIELD, item.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(VERSION_FIELD, item.versionNumber.toString(), Store.YES, Index.ANALYZED))
+        document.add(new Field(MODEL_CLASS_FIELD, item.modelClass.name, Store.YES, Index.ANALYZED))
         document.add(new Field(ITEM_TYPE_FIELD, ItemType.RESULT_CONFIGURATION.toString(), Store.YES, Index.ANALYZED))
+        addFieldIfNotNull(document, OWNER_FIELD, item.creator?.username?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_BY, item.lastUpdater?.username?.toString())
+        addFieldIfNotNull(document, CREATION_TIME, item.creationDate?.millis?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_TIME, item.modificationDate?.millis?.toString())
 
         return document
     }
 
-    private static Document doCreateDocument(Simulation simulation) {
+    private static Document doCreateDocument(Simulation item) {
         Document document = new Document()
-        document.add(new Field(NAME_FIELD, simulation.name, Store.YES, Index.ANALYZED))
-        document.add(new Field(MODEL_CLASS_FIELD, simulation.modelClass.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(NAME_FIELD, item.name, Store.YES, Index.ANALYZED))
+        document.add(new Field(MODEL_CLASS_FIELD, item.modelClass.name, Store.YES, Index.ANALYZED))
         document.add(new Field(ITEM_TYPE_FIELD, ItemType.RESULT.toString(), Store.YES, Index.ANALYZED))
-        document.add(new Field(TAGS_FIELD, simulation.tags*.name.join(" "), Store.NO, Index.ANALYZED))
+        document.add(new Field(TAGS_FIELD, item.tags*.name.join(" "), Store.YES, Index.ANALYZED))
+        addFieldIfNotNull(document, OWNER_FIELD, item.creator?.username?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_BY, item.lastUpdater?.username?.toString())
+        addFieldIfNotNull(document, CREATION_TIME, item.creationDate?.millis?.toString())
+        addFieldIfNotNull(document, LASTUPDATED_TIME, item.modificationDate?.millis?.toString())
 
         return document
     }
@@ -86,31 +127,67 @@ abstract class DocumentFactory {
         String name = document.getField(NAME_FIELD).stringValue()
         Class modelClass = DocumentFactory.getClassLoader().loadClass(document.getField(MODEL_CLASS_FIELD).stringValue())
         switch (itemType) {
-            //TODO (PMO-2441) put all fields for table tree into index to avoid item loading.
             case ItemType.PARAMETERIZATION:
                 VersionNumber versionNumber = new VersionNumber(document.getField(VERSION_FIELD).stringValue())
                 Parameterization parameterization = new Parameterization(name, modelClass)
                 parameterization.versionNumber = versionNumber
-                parameterization.load(false)
+                parameterization.valid = document.getField(VALID_FIELD).stringValue().toBoolean()
+                parameterization.status = Status.valueOf(document.getField(STATE_FIELD).stringValue())
+                mapOwner(parameterization, document)
+                mapModificator(parameterization, document)
+                mapTags(parameterization, document)
                 return parameterization
             case ItemType.RESULT_CONFIGURATION:
                 VersionNumber versionNumber = new VersionNumber(document.getField(VERSION_FIELD).stringValue())
                 ResultConfiguration resultConfiguration = new ResultConfiguration(name)
                 resultConfiguration.modelClass = modelClass
                 resultConfiguration.versionNumber = versionNumber
-                resultConfiguration.load(false)
+                mapOwner(resultConfiguration, document)
+                mapModificator(resultConfiguration, document)
                 return resultConfiguration
             case ItemType.RESULT:
                 Simulation simulation = new Simulation(name)
                 simulation.modelClass = modelClass
-                simulation.load(false)
+                mapOwner(simulation, document)
+                mapModificator(simulation, document)
+                mapTags(simulation, document)
                 return simulation
             case ItemType.RESOURCE:
                 Resource resource = new Resource(name, modelClass)
                 resource.versionNumber = new VersionNumber(document.getField(VERSION_FIELD).stringValue())
-                resource.load(false)
+                resource.valid = document.getField(VALID_FIELD).stringValue().toBoolean()
+                resource.status = Status.valueOf(document.getField(STATE_FIELD).stringValue())
+                mapOwner(resource, document)
+                mapModificator(resource, document)
+                mapTags(resource, document)
                 return resource
 
+        }
+    }
+
+    private static void mapTags(ModellingItem item, Document document) {
+        Field field = document.getField(TAGS_FIELD)
+        if (field) {
+            List<String> tags = field.stringValue().split(' ').toList()
+            tags.each {
+                item.tags << new Tag(name:it)
+            }
+        }
+    }
+
+    private static void mapOwner(ModellingItem item, Document document) {
+        item.creationDate = new DateTime(document.getField(CREATION_TIME).stringValue().toLong())
+        Field field = document.getField(OWNER_FIELD)
+        if (field) {
+            item.creator = new Person(username: field.stringValue())
+        }
+    }
+
+    private static void mapModificator(ModellingItem item, Document document) {
+        item.modificationDate = new DateTime(document.getField(LASTUPDATED_TIME).stringValue().toLong())
+        Field field = document.getField(LASTUPDATED_BY)
+        if (field) {
+            item.lastUpdater = new Person(username: field.stringValue())
         }
     }
 
---
+++ test/unit/org/pillarone/riskanalytics/application/search/DocumentFactoryTest.groovy
@@ -0,0 +1,153 @@
+package org.pillarone.riskanalytics.application.search
+
+import org.apache.lucene.document.Document
+import org.joda.time.DateTime
+import org.pillarone.riskanalytics.core.parameter.comment.Tag
+import org.pillarone.riskanalytics.core.simulation.item.ModellingItem
+import org.pillarone.riskanalytics.core.simulation.item.Parameterization
+import org.pillarone.riskanalytics.core.simulation.item.Resource
+import org.pillarone.riskanalytics.core.simulation.item.ResultConfiguration
+import org.pillarone.riskanalytics.core.simulation.item.Simulation
+import org.pillarone.riskanalytics.core.simulation.item.VersionNumber
+import org.pillarone.riskanalytics.core.user.Person
+import org.pillarone.riskanalytics.core.workflow.Status
+
+class DocumentFactoryTest extends GroovyTestCase {
+
+    void testDocumentCreation_Parameterization() {
+        Parameterization parameterization = createParameterization()
+        Document document = DocumentFactory.createDocument(parameterization)
+        assertEquals(11, document.fields.size())
+        assertCommonFields(document, parameterization, DocumentFactory.ItemType.PARAMETERIZATION)
+        assertNotNull(document.getField(DocumentFactory.VERSION_FIELD))
+        assertEquals(parameterization.versionNumber.toString(), document.getField(DocumentFactory.VERSION_FIELD).stringValue())
+        assertNotNull(document.getField(DocumentFactory.VALID_FIELD))
+        assertEquals(parameterization.valid.toString(), document.getField(DocumentFactory.VALID_FIELD).stringValue())
+        assertNotNull(document.getField(DocumentFactory.TAGS_FIELD))
+        assertEquals(parameterization.tags.join(' '), document.getField(DocumentFactory.TAGS_FIELD).stringValue())
+        assertNotNull(document.getField(DocumentFactory.STATE_FIELD))
+        assertEquals(parameterization.status.toString(), document.getField(DocumentFactory.STATE_FIELD).stringValue())
+        assertNotNull(document.getField(DocumentFactory.OWNER_FIELD))
+        assertEquals(parameterization.creator.username, document.getField(DocumentFactory.OWNER_FIELD).stringValue())
+        assertNotNull(document.getField(DocumentFactory.LASTUPDATED_BY))
+        assertEquals(parameterization.lastUpdater.username, document.getField(DocumentFactory.LASTUPDATED_BY).stringValue())
+    }
+
+    void testBothWays_Parameterization() {
+        Parameterization parameterization = createParameterization()
+        Document document = DocumentFactory.createDocument(parameterization)
+        Parameterization fromDocumentFactory = DocumentFactory.toModellingItem(document)
+        assertEquals(parameterization.name, fromDocumentFactory.name)
+        assertEquals(parameterization.modelClass, fromDocumentFactory.modelClass)
+        assertEquals(parameterization.versionNumber, fromDocumentFactory.versionNumber)
+        assertEquals(parameterization.tags, fromDocumentFactory.tags)
+        assertEquals(parameterization.creator.username, fromDocumentFactory.creator.username)
+        assertEquals(parameterization.creationDate, fromDocumentFactory.creationDate)
+        assertEquals(parameterization.lastUpdater.username, fromDocumentFactory.lastUpdater.username)
+        assertEquals(parameterization.modificationDate, fromDocumentFactory.modificationDate)
+        assertEquals(parameterization.valid, fromDocumentFactory.valid)
+        assertEquals(parameterization.status, fromDocumentFactory.status)
+    }
+    void testBothWays_Resource() {
+        Resource resource = createResource()
+        Document document = DocumentFactory.createDocument(resource)
+        Resource fromDocumentFactory = DocumentFactory.toModellingItem(document)
+        assertEquals(resource.name, fromDocumentFactory.name)
+        assertEquals(resource.modelClass, fromDocumentFactory.modelClass)
+        assertEquals(resource.versionNumber, fromDocumentFactory.versionNumber)
+        assertEquals(resource.tags, fromDocumentFactory.tags)
+        assertEquals(resource.creator.username, fromDocumentFactory.creator.username)
+        assertEquals(resource.creationDate, fromDocumentFactory.creationDate)
+        assertEquals(resource.lastUpdater.username, fromDocumentFactory.lastUpdater.username)
+        assertEquals(resource.modificationDate, fromDocumentFactory.modificationDate)
+        assertEquals(resource.valid, fromDocumentFactory.valid)
+        assertEquals(resource.status, fromDocumentFactory.status)
+    }
+
+    void testBothWays_Simulation() {
+        Simulation simulation = createSimulation()
+        Document document = DocumentFactory.createDocument(simulation)
+        Simulation fromDocumentFactory = DocumentFactory.toModellingItem(document)
+        assertEquals(simulation.name, fromDocumentFactory.name)
+        assertEquals(simulation.modelClass, fromDocumentFactory.modelClass)
+        assertEquals(simulation.tags, fromDocumentFactory.tags)
+        assertEquals(simulation.creator.username, fromDocumentFactory.creator.username)
+        assertEquals(simulation.creationDate, fromDocumentFactory.creationDate)
+        assertEquals(simulation.lastUpdater.username, fromDocumentFactory.lastUpdater.username)
+        assertEquals(simulation.modificationDate, fromDocumentFactory.modificationDate)
+    }
+
+    void testBothWays_ResultConfiguration() {
+        ResultConfiguration resultConfiguration = createResultConfiguration()
+        Document document = DocumentFactory.createDocument(resultConfiguration)
+        ResultConfiguration fromDocumentFactory = DocumentFactory.toModellingItem(document)
+        assertEquals(resultConfiguration.name, fromDocumentFactory.name)
+        assertEquals(resultConfiguration.modelClass, fromDocumentFactory.modelClass)
+        assertEquals(resultConfiguration.creator.username, fromDocumentFactory.creator.username)
+        assertEquals(resultConfiguration.creationDate, fromDocumentFactory.creationDate)
+        assertEquals(resultConfiguration.lastUpdater.username, fromDocumentFactory.lastUpdater.username)
+        assertEquals(resultConfiguration.modificationDate, fromDocumentFactory.modificationDate)
+    }
+
+    private ResultConfiguration createResultConfiguration() {
+        ResultConfiguration resultConfiguration = new ResultConfiguration("resultConfiguration")
+        resultConfiguration.modelClass = DocumentFactory
+        resultConfiguration.creator = new Person(username: 'Owner')
+        resultConfiguration.lastUpdater = new Person(username: 'Updater')
+        resultConfiguration.creationDate = new DateTime(System.currentTimeMillis() - 1000)
+        resultConfiguration.modificationDate = new DateTime(System.currentTimeMillis())
+        return resultConfiguration
+    }
+
+    private Simulation createSimulation() {
+        Simulation simulation = new Simulation("simulation")
+        simulation.modelClass = DocumentFactory
+        simulation.creator = new Person(username: 'Owner')
+        simulation.lastUpdater = new Person(username: 'Updater')
+        simulation.creationDate = new DateTime(System.currentTimeMillis() - 1000)
+        simulation.modificationDate = new DateTime(System.currentTimeMillis())
+        simulation.tags = [new Tag(name: 'A'), new Tag(name: 'B'), new Tag(name: 'C')]
+        return simulation
+    }
+
+    private Parameterization createParameterization() {
+        Parameterization parameterization = new Parameterization("test", DocumentFactory)
+        parameterization.valid = true
+        parameterization.creator = new Person(username: 'Owner')
+        parameterization.lastUpdater = new Person(username: 'Updater')
+        parameterization.creationDate = new DateTime(System.currentTimeMillis() - 1000)
+        parameterization.modificationDate = new DateTime(System.currentTimeMillis())
+        parameterization.versionNumber = new VersionNumber('13')
+        parameterization.status = Status.DATA_ENTRY
+        parameterization.tags = [new Tag(name: 'A'), new Tag(name: 'B'), new Tag(name: 'C')]
+        parameterization
+    }
+
+    private Resource createResource() {
+        Resource resource = new Resource("test", DocumentFactory)
+        resource.valid = true
+        resource.creator = new Person(username: 'Owner')
+        resource.lastUpdater = new Person(username: 'Updater')
+        resource.creationDate = new DateTime(System.currentTimeMillis() - 1000)
+        resource.modificationDate = new DateTime(System.currentTimeMillis())
+        resource.versionNumber = new VersionNumber('13')
+        resource.status = Status.DATA_ENTRY
+        resource.tags = [new Tag(name: 'A'), new Tag(name: 'B'), new Tag(name: 'C')]
+        resource
+    }
+
+    void assertCommonFields(Document document, ModellingItem item, DocumentFactory.ItemType type) {
+        assertNotNull(document.getField(DocumentFactory.NAME_FIELD))
+        assertNotNull(document.getField(DocumentFactory.ITEM_TYPE_FIELD))
+        assertNotNull(document.getField(DocumentFactory.MODEL_CLASS_FIELD))
+        assertNotNull(document.getField(DocumentFactory.CREATION_TIME))
+        assertNotNull(document.getField(DocumentFactory.LASTUPDATED_TIME))
+        assertEquals(item.name, document.getField(DocumentFactory.NAME_FIELD).stringValue())
+        assertEquals(item.modelClass.name, document.getField(DocumentFactory.MODEL_CLASS_FIELD).stringValue())
+        assertEquals(item.creationDate, new DateTime(document.getField(DocumentFactory.CREATION_TIME).stringValue().toLong()))
+        assertEquals(item.modificationDate, new DateTime(document.getField(DocumentFactory.LASTUPDATED_TIME).stringValue().toLong()))
+        assertEquals(type.toString(), document.getField(DocumentFactory.ITEM_TYPE_FIELD).stringValue())
+
+
+    }
+}
