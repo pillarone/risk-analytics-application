3d3b05ccdf1b219ddc1b07a3680a618827cd360f
detlef-brendle <detlef.brendle@canoo.com>
2013-07-17 12:58:54 +0200

    PMO-2554 - Check Workflow Action functionality register ULCSession to service.

--- src/groovy/org/pillarone/riskanalytics/application/environment/P1RATViewFactory.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/environment/P1RATViewFactory.groovy
@@ -2,26 +2,24 @@ package org.pillarone.riskanalytics.application.environment
 
 import com.canoo.ulc.community.locale.server.ULCClientTimeZoneSetter
 import com.canoo.ulc.community.ulcclipboard.server.ULCClipboard
+import com.ulcjava.applicationframework.application.ApplicationContext
 import com.ulcjava.base.application.BorderFactory
 import com.ulcjava.base.application.ClientContext
-import com.ulcjava.base.application.ULCBoxPane
-import com.ulcjava.base.application.ULCCardPane
-import com.ulcjava.base.application.ULCComponent
 import com.ulcjava.base.application.ULCRootPane
-import com.ulcjava.base.application.border.ULCAbstractBorder
 import com.ulcjava.base.application.util.BorderedComponentUtilities
+import com.ulcjava.base.server.ULCSession
 import com.ulcjava.base.shared.IDefaults
 import com.ulcjava.container.grails.UlcViewFactory
+import grails.util.Holders
 import groovy.transform.CompileStatic
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
 import org.apache.log4j.MDC
 import org.pillarone.riskanalytics.application.UserContext
+import org.pillarone.riskanalytics.application.search.ModellingItemSearchService
 import org.pillarone.riskanalytics.application.ui.main.view.RiskAnalyticsMainModel
 import org.pillarone.riskanalytics.application.ui.main.view.RiskAnalyticsMainView
 import org.pillarone.riskanalytics.application.ui.util.UIUtils
-import org.pillarone.riskanalytics.application.search.ModellingItemSearchService
-import com.ulcjava.applicationframework.application.ApplicationContext
 
 //used for Applet & JNLP (but not standalone)
 @CompileStatic
@@ -38,8 +36,7 @@ abstract class P1RATViewFactory implements UlcViewFactory {
         } catch (Exception ex) {
             // put a user in MDC causes an exception in integration Test
         }
-        //TODO: re-enable when it's actually used, otherwise it will initialize here and delay the first startup after re-deploymemt
-//        searchService = ApplicationHolder.application.mainContext.getBean(ModellingItemSearchService)
+        searchService = Holders.getApplicationContext().getBean(ModellingItemSearchService)
 
         UserContext.setUserTimeZone(ClientContext.timeZone)
         ULCClientTimeZoneSetter.setDefaultTimeZone(TimeZone.getTimeZone("UTC"))
@@ -50,7 +47,7 @@ abstract class P1RATViewFactory implements UlcViewFactory {
         RiskAnalyticsMainView mainView = new RiskAnalyticsMainView(new RiskAnalyticsMainModel(applicationContext: applicationContext))
         mainView.init()
 
-//        searchService.registerSession(ULCSession.currentSession())
+        searchService.registerSession(ULCSession.currentSession())
 
         frame.setMenuBar(mainView.getMenuBar())
         frame.add(BorderedComponentUtilities.createBorderedComponent(mainView.content, IDefaults.BOX_EXPAND_EXPAND, BorderFactory.createEmptyBorder(5, 5, 5, 5)))
@@ -61,7 +58,7 @@ abstract class P1RATViewFactory implements UlcViewFactory {
     abstract protected ULCRootPane createRootPane()
 
     void stop() {
-//        searchService.unregisterSession(ULCSession.currentSession())
+        searchService.unregisterSession(ULCSession.currentSession())
     }
 
 }
