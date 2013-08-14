bfdef418a6f11190a89a6dbff94afe6c66a9860c
detlef-brendle <detlef.brendle@canoo.com>
2013-08-14 13:26:18 +0200

    upgrade to ulc-extension 1.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,7 +37,7 @@ grails.project.dependency.resolution = {
         runtime ":spring-security-core:1.2.7.3"
 
         compile "com.canoo:ulc:${ulcVersion}"
-        runtime("org.pillarone:pillar-one-ulc-extensions:0.9") { transitive = false }
+        runtime("org.pillarone:pillar-one-ulc-extensions:1.0") { transitive = false }
 
         test ":code-coverage:1.2.4"
 
