38f426d13aa963c688a7f4ea314713bfb158c54f
detlef-brendle <detlef.brendle@canoo.com>
2013-10-01 17:28:20 +0200

    PMO-2449 - handle different mdps

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/action/exportimport/ExcelExportHandler.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/action/exportimport/ExcelExportHandler.groovy
@@ -2,9 +2,9 @@ package org.pillarone.riskanalytics.application.ui.main.action.exportimport
 
 import org.apache.poi.POIXMLProperties
 import org.apache.poi.ss.usermodel.*
+import org.apache.poi.xssf.usermodel.XSSFHyperlink
 import org.apache.poi.xssf.usermodel.XSSFRichTextString
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import org.pillarone.riskanalytics.application.ui.parameterization.model.TreeBuilderUtil
 import org.pillarone.riskanalytics.application.ui.util.I18NUtils
 import org.pillarone.riskanalytics.core.components.Component
 import org.pillarone.riskanalytics.core.components.ComponentUtils
@@ -12,13 +12,14 @@ import org.pillarone.riskanalytics.core.components.ComposedComponent
 import org.pillarone.riskanalytics.core.components.DynamicComposedComponent
 import org.pillarone.riskanalytics.core.model.Model
 import org.pillarone.riskanalytics.core.parameterization.ConstrainedMultiDimensionalParameter
+import org.pillarone.riskanalytics.core.parameterization.IMultiDimensionalConstraints
 import org.pillarone.riskanalytics.core.parameterization.IParameterObject
 import org.pillarone.riskanalytics.core.parameterization.IParameterObjectClassifier
 import org.pillarone.riskanalytics.core.util.PropertiesUtils
 
 class ExcelExportHandler extends AbstractExcelHandler {
     Model model
-    List<ConstrainedMultiDimensionalParameter> multiDimensionalParameters = []
+    Map<IMultiDimensionalConstraints, List<MDPTitleContraints>> mdpConstraintsWithTitles = [:]
 
     ExcelExportHandler(Model model) {
         super()
@@ -46,15 +47,17 @@ class ExcelExportHandler extends AbstractExcelHandler {
                 }
             }
         }
-        multiDimensionalParameters.each { ConstrainedMultiDimensionalParameter mdp ->
-            String mdpSheetName = getSheetName(mdp)
-            Sheet sheet = workbook.createSheet(mdpSheetName)
-            properties.addProperty(mdpSheetName, sheet.sheetName)
-            Row headerRow = sheet.createRow(0)
-            headerRow.createCell(0).setCellValue('Link to component todo...')
-            Row columnNameRow = sheet.createRow(1)
-            mdp.getColumnNames().eachWithIndex { val, i ->
-                columnNameRow.createCell(i).setCellValue(val)
+        mdpConstraintsWithTitles.each { Map.Entry<IMultiDimensionalConstraints, List<MDPTitleContraints>> entry ->
+            entry.value.eachWithIndex { MDPTitleContraints constraints, int index ->
+                String mdpSheetName = getSheetName(constraints)
+                Sheet sheet = workbook.createSheet(mdpSheetName)
+                properties.addProperty(mdpSheetName, sheet.sheetName)
+                Row headerRow = sheet.createRow(0)
+                headerRow.createCell(0).setCellValue('Link to component todo...')
+                Row columnNameRow = sheet.createRow(1)
+                constraints.titles.eachWithIndex { String val, i ->
+                    columnNameRow.createCell(i).setCellValue(val)
+                }
             }
         }
         workbook.write(outputStream)
@@ -69,14 +72,18 @@ class ExcelExportHandler extends AbstractExcelHandler {
             Cell cell = headerRow.createCell(columnIndex, Cell.CELL_TYPE_STRING)
             setFont(cell, 10 as short, false, Font.BOLDWEIGHT_BOLD)
             setFont(technicalCell, 10 as short, false, Font.BOLDWEIGHT_BOLD)
-            String displayName = I18NUtils.findParameterDisplayName(component, parm)
-            //TODO: call normalizedName
-            cell.setCellValue(displayName ?: ComponentUtils.getNormalizedName(parm))
+            cell.setCellValue(getDisplayName(component,parm))
             technicalCell.setCellValue(parm)
-            columnIndex = addParameterCells(component[parm], headerRow, technicalHeaderRow, ++columnIndex)
+
+            columnIndex = addParameterCells(component[parm], headerRow, technicalHeaderRow, ++columnIndex, cell)
         }
 
         return columnIndex
+    }
+
+    private static String getDisplayName(Component component,String name) {
+        String displayName = I18NUtils.findParameterDisplayName(component, name)
+        return displayName ?: ComponentUtils.getNormalizedName(name)
     }
 
     private int handleComponent(ComposedComponent component, Row headerRow, Row technicalHeaderRow, int columnIndex) {
@@ -85,8 +92,7 @@ class ExcelExportHandler extends AbstractExcelHandler {
             String propertyName = component.properties.entrySet().find { it.value == subComponent }.key
             Cell technicalCell = technicalHeaderRow.createCell(columnIndex)
             Cell cell = headerRow.createCell(columnIndex)
-            String displayName = I18NUtils.findParameterDisplayName(component, propertyName)
-            cell.setCellValue(displayName ?: propertyName)
+            cell.setCellValue(getDisplayName(component,propertyName))
             technicalCell.setCellValue(propertyName)
             columnIndex = handleComponent(subComponent, headerRow, technicalHeaderRow, ++columnIndex)
         }
@@ -107,16 +113,17 @@ class ExcelExportHandler extends AbstractExcelHandler {
         return handleComponent(component.createDefaultSubComponent(), headerRow, technicalHeaderRow, ++columnIndex)
     }
 
-    private int addParameterCells(def parmObject, Row headerRow, Row technicalHeaderRow, int columnIndex) {
+    private int addParameterCells(def parmObject, Row headerRow, Row technicalHeaderRow, int columnIndex, Cell cell) {
         columnIndex
     }
 
-    private int addParameterCells(ConstrainedMultiDimensionalParameter multiDimensionalParameter, Row headerRow, Row technicalHeaderRow, int columnIndex) {
+    private int addParameterCells(ConstrainedMultiDimensionalParameter multiDimensionalParameter, Row headerRow, Row technicalHeaderRow, int columnIndex, Cell cell) {
         addParameter(multiDimensionalParameter)
+        setCellComment(cell, getSheetName(new MDPTitleContraints(multiDimensionalParameter.titles, multiDimensionalParameter.constraints)))
         return columnIndex
     }
 
-    private int addParameterCells(IParameterObject parmObject, Row headerRow, Row technicalHeaderRow, int columnIndex) {
+    private int addParameterCells(IParameterObject parmObject, Row headerRow, Row technicalHeaderRow, int columnIndex, Cell c) {
         List<IParameterObjectClassifier> classifiers = parmObject.type.getClassifiers()
         Set writtenParameters = []
         classifiers.each { IParameterObjectClassifier classifier ->
@@ -132,16 +139,19 @@ class ExcelExportHandler extends AbstractExcelHandler {
 
                     Object classifierParameter = classifier.getType(parmName)
                     if (classifierParameter instanceof ConstrainedMultiDimensionalParameter) {
-                        setCellComment(cell, getSheetName(classifierParameter))
-                        //TODO
-//                        cell.setCellValue("=HYPERLINK(\"#Exampleresourceconstraints-MDP\";\"retrospectivereinsurance\")")
                         addParameter(classifierParameter)
+                        String sheetName = getSheetName(new MDPTitleContraints(classifierParameter.titles, classifierParameter.constraints))
+                        setCellComment(cell, sheetName)
+                        //TODO
+                        Hyperlink hyperlink = cell.sheet.workbook.creationHelper.createHyperlink(Hyperlink.LINK_DOCUMENT)
+                        hyperlink.setAddress(sheetName)
+                        cell.setHyperlink(hyperlink)
 
                     }
                     if (parmObject.class == classifierParameter.class) {
                         // TODO (recursive call with same classifier not supported.)
                     } else {
-                        columnIndex = addParameterCells(classifierParameter, headerRow, technicalHeaderRow, ++columnIndex)
+                        columnIndex = addParameterCells(classifierParameter, headerRow, technicalHeaderRow, ++columnIndex, cell)
 
                     }
                 }
@@ -151,14 +161,22 @@ class ExcelExportHandler extends AbstractExcelHandler {
         return columnIndex
     }
 
-    private static String getSheetName(ConstrainedMultiDimensionalParameter constrainedMultiDimensionalParameter) {
-        String name = "${constrainedMultiDimensionalParameter.constraints.class.simpleName}-MDP"
+    private String getSheetName(MDPTitleContraints mdpTitleContraints) {
+        List<MDPTitleContraints> mdpsForConstraints = mdpConstraintsWithTitles.get(mdpTitleContraints.constraints)
+        String counter = mdpsForConstraints.indexOf(mdpTitleContraints)
+        String name = "MDP${counter}-${mdpTitleContraints.constraints.class.simpleName}"
         return name.substring(0, Math.min(name.length(), 31));
     }
 
     private addParameter(ConstrainedMultiDimensionalParameter multiDimensionalParameter) {
-        if (!multiDimensionalParameters*.constraints.contains(multiDimensionalParameter.constraints)) {
-            multiDimensionalParameters << multiDimensionalParameter
+        MDPTitleContraints mdpConstraintWithTitle = new MDPTitleContraints(multiDimensionalParameter.titles, multiDimensionalParameter.constraints)
+        if (!mdpConstraintsWithTitles.values().contains(mdpConstraintWithTitle)) {
+            if (!mdpConstraintsWithTitles.containsKey(multiDimensionalParameter.constraints)) {
+                mdpConstraintsWithTitles.put(multiDimensionalParameter.constraints, [])
+            }
+            if (!mdpConstraintsWithTitles.get(multiDimensionalParameter.constraints).contains(mdpConstraintWithTitle)) {
+                mdpConstraintsWithTitles.get(multiDimensionalParameter.constraints).add(mdpConstraintWithTitle)
+            }
         }
 
     }
@@ -184,5 +202,33 @@ class ExcelExportHandler extends AbstractExcelHandler {
         cell.setCellComment(comment)
     }
 
+    class MDPTitleContraints {
+        List<String> titles
+        IMultiDimensionalConstraints constraints
+
+        MDPTitleContraints(List<String> titles, IMultiDimensionalConstraints constraints) {
+            this.titles = titles
+            this.constraints = constraints
+        }
+
+        boolean equals(o) {
+            if (this.is(o)) return true
+            if (getClass() != o.class) return false
+
+            MDPTitleContraints that = (MDPTitleContraints) o
+
+            if (constraints != that.constraints) return false
+            if (titles != that.titles) return false
+
+            return true
+        }
+
+        int hashCode() {
+            int result
+            result = (titles != null ? titles.hashCode() : 0)
+            result = 31 * result + (constraints != null ? constraints.hashCode() : 0)
+            return result
+        }
+    }
 
 }
