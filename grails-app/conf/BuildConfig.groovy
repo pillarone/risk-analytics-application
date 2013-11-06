c262df32f5f7dc8b5bd3d5709e8a1acc8cb81c5a
detlef-brendle <detlef.brendle@canoo.com>
2013-11-06 13:41:04 +0100

    build configuration adjustments

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,6 +5,9 @@ grails.project.plugins.dir = "../local-plugins/RiskAnalyticsApplication-master"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 
+grails.project.dependency.resolver = "maven"
+grails.project.fork = [test: false, run: false]
+
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn"
@@ -14,13 +17,8 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenCentral()
 
-        def ulcClientJarResolver = new FileSystemResolver()
-        ulcClientJarResolver.addArtifactPattern "${basedir}/web-app/lib/[artifact]-[revision].[ext]"
-        ulcClientJarResolver.name = "ulc"
-
-        resolver ulcClientJarResolver
-
         mavenRepo "https://repository.intuitive-collaboration.com/nexus/content/repositories/pillarone-public/"
+        mavenRepo "http://repo.spring.io/milestone/" //needed for spring-security-core 2.0-rc2 plugin
         mavenRepo "https://ci.canoo.com/nexus/content/repositories/public-releases"
     }
 
@@ -28,36 +26,34 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":background-thread:1.3"
-        runtime ":hibernate:2.2.1"
+        runtime ":hibernate:3.6.10.2"
         runtime ":joda-time:0.5"
-        runtime ":maven-publisher:0.7.5", {
+        runtime ":release:3.0.1", {
             excludes "groovy"
         }
         runtime ":quartz:0.4.2"
-        runtime ":spring-security-core:1.2.7.3"
+        runtime ":spring-security-core:2.0-RC2"
 
         compile "com.canoo:ulc:${ulcVersion}"
-        runtime("org.pillarone:pillar-one-ulc-extensions:1.1") { transitive = false }
+        runtime("org.pillarone:pillar-one-ulc-extensions:1.2") { transitive = false }
 
-        test ":code-coverage:1.2.4"
+        test ":code-coverage:1.2.6"
 
         if (appName == 'RiskAnalyticsApplication') {
-            runtime "org.pillarone:risk-analytics-core:1.8"
+            runtime "org.pillarone:risk-analytics-core:1.9-a1"
         }
 
     }
 
     dependencies {
         runtime "mysql:mysql-connector-java:5.1.20"
-        compile group: 'canoo', name: 'ULCMigLayout-client', version: "1.0"
-        compile group: 'canoo', name: 'miglayout', version: "3.7.3.1"
+        compile (group: 'com.canoo.ulc.ext.ULCMigLayout', name: 'ULCMigLayout-client', version: "1.0") { transitive = false }
+        compile group: 'com.miglayout', name: 'miglayout', version: "3.7.3.1"
+
 
         //required for ulc tests
         test 'org.mortbay.jetty:jetty:6.1.21', 'org.mortbay.jetty:jetty-plus:6.1.21'
         test 'org.mortbay.jetty:jetty-util:6.1.21', 'org.mortbay.jetty:jetty-naming:6.1.21'
-        test('org.mortbay.jetty:jsp-2.0:6.1.21') {
-            excludes 'commons-el', 'ant', 'slf4j-api', 'slf4j-simple', 'jcl104-over-slf4j', 'xercesImpl', 'xmlParserAPIs'
-        }
         test 'hsqldb:hsqldb:1.8.0.10'
     }
 
