ddf27cd8818b04a9f2541fcc34a1fc28f945e0f3
detlef-brendle <detlef.brendle@canoo.com>
2013-11-28 16:48:54 +0100

    PMO-2572 issues must also show attached comment files

--- src/groovy/org/pillarone/riskanalytics/application/ui/comment/view/NewIssueView.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/comment/view/NewIssueView.groovy
@@ -21,14 +21,4 @@ class NewIssueView extends NewCommentView {
     protected Comment createComment(String path, int periodIndex, String function = null) {
         return new WorkflowComment(path, periodIndex)
     }
-
-    protected void layoutComponents() {
-        content.add(ULCBoxPane.BOX_LEFT_TOP, UIUtils.spaceAround(new ULCScrollPane(commentTextArea), 5, 5, 0, 0))
-        content.add(2, ULCBoxPane.BOX_EXPAND_EXPAND, new ULCFiller());
-        content.add(ULCBoxPane.BOX_LEFT_TOP, getButtonsPane())
-        content.add(2, ULCBoxPane.BOX_EXPAND_EXPAND, new ULCFiller());
-        content.add(3, ULCBoxPane.BOX_EXPAND_EXPAND, new ULCFiller());
-    }
-
-
 }
