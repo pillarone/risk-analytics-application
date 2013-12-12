1046176b7a2da43c440ae3e7cec78eae53654df9
detlef-brendle <detlef.brendle@canoo.com>
2013-12-12 15:45:52 +0100

    new version 1.8.20, test for ART-1186 added

--- test/integration/org/pillarone/riskanalytics/application/ui/interaction/WorkflowActionTests.groovy
+++ test/integration/org/pillarone/riskanalytics/application/ui/interaction/WorkflowActionTests.groovy
@@ -1,15 +1,23 @@
 package org.pillarone.riskanalytics.application.ui.interaction
 
 import com.ulcjava.testframework.operator.ComponentByNameChooser
+import com.ulcjava.testframework.operator.ULCButtonOperator
+import com.ulcjava.testframework.operator.ULCDialogOperator
 import com.ulcjava.testframework.operator.ULCFrameOperator
+import com.ulcjava.testframework.operator.ULCMenuItemOperator
 import com.ulcjava.testframework.operator.ULCPopupMenuOperator
+import com.ulcjava.testframework.operator.ULCTableTreeOperator
+import com.ulcjava.testframework.operator.ULCTextAreaOperator
 import com.ulcjava.testframework.operator.ULCTreeOperator
 import com.ulcjava.testframework.standalone.AbstractStandaloneTestCase
+import org.pillarone.riskanalytics.application.UserContext
 import org.pillarone.riskanalytics.application.dataaccess.item.ModellingItemFactory
 import org.pillarone.riskanalytics.application.ui.P1RATApplication
 import org.pillarone.riskanalytics.application.util.LocaleResources
 import org.pillarone.riskanalytics.core.fileimport.FileImportService
-import org.pillarone.riskanalytics.core.output.DBCleanUpService
+import org.pillarone.riskanalytics.core.model.registry.ModelRegistry
+import org.pillarone.riskanalytics.core.workflow.AuditLog
+
 import javax.swing.tree.TreePath
 
 class WorkflowActionTests extends AbstractStandaloneTestCase {
@@ -18,7 +26,10 @@ class WorkflowActionTests extends AbstractStandaloneTestCase {
         FileImportService.importModelsIfNeeded(["Application"])
         ModellingItemFactory.clear()
         LocaleResources.setTestMode()
-        super.setUp();
+        super.setUp()
+        UserContext.metaClass.static.hasCurrentUser = {->
+            true
+        }
     }
 
     protected void tearDown() {
@@ -30,39 +41,57 @@ class WorkflowActionTests extends AbstractStandaloneTestCase {
         return P1RATApplication
     }
 
-    //TODO: refactor with several roles
-    void testWorkflow() {
-        return
+    void testDeleteP14NInWorkflow() {
         ULCFrameOperator frame = new ULCFrameOperator("Risk Analytics")
-        ULCTreeOperator tree = new ULCTreeOperator(frame, new ComponentByNameChooser("selectionTree"))
+        ULCTableTreeOperator itemTree = new ULCTableTreeOperator(frame, new ComponentByNameChooser("selectionTreeRowHeader"))
+        TreePath applicationModelPath = itemTree.findPath('Application')
+        int applicationModelRow = itemTree.getRowForPath(applicationModelPath)
+        itemTree.doExpandRow(applicationModelRow)
+        itemTree.doExpandRow(applicationModelRow + 1)
+        itemTree.selectRow(applicationModelRow + 2)
+        ULCPopupMenuOperator popupMenu = itemTree.callPopupOnCell(applicationModelRow + 2, 0)
+        ULCMenuItemOperator startWorkflow = new ULCMenuItemOperator(popupMenu, "Start workflow")
+        startWorkflow.clickMouse()
+        ULCDialogOperator dealLink = new ULCDialogOperator(new ComponentByNameChooser('dealDialog'))
+        new ULCButtonOperator(dealLink, new ComponentByNameChooser('okButton')).clickMouse()
 
-        TreePath pathForRename = tree.findPath(["Application", "Parameterization", "Normal", "ApplicationParameters"] as String[])
-        assertNotNull "path not found", pathForRename
+        ULCDialogOperator newVersion = new ULCDialogOperator(new ComponentByNameChooser('renameDialog'))
+        ULCTextAreaOperator commentTextArea = new ULCTextAreaOperator(newVersion, new ComponentByNameChooser('commentTextArea'))
+        commentTextArea.enterText('test')
+        new ULCButtonOperator(newVersion, new ComponentByNameChooser('okButton')).clickMouse()
+        sleep(2000)
+        itemTree.selectRow(3)
+        popupMenu = itemTree.callPopupOnCell(applicationModelRow + 3, 0)
+        ULCMenuItemOperator sendToReview = new ULCMenuItemOperator(popupMenu, "Send to reviewer")
+        sendToReview.clickMouse()
+        sleep(2000)
+        popupMenu = itemTree.callPopupOnCell(applicationModelRow + 3, 0)
+        ULCMenuItemOperator sendToProduction = new ULCMenuItemOperator(popupMenu, "Send to production")
+        sendToProduction.clickMouse()
+        sleep(2000)
+        popupMenu = itemTree.callPopupOnCell(applicationModelRow + 3, 0)
+        ULCMenuItemOperator createNewVersion = new ULCMenuItemOperator(popupMenu, "Create new version")
+        createNewVersion.clickMouse()
+        newVersion = new ULCDialogOperator(new ComponentByNameChooser('renameDialog'))
+        commentTextArea = new ULCTextAreaOperator(newVersion, new ComponentByNameChooser('commentTextArea'))
+        commentTextArea.enterText('test')
+        new ULCButtonOperator(newVersion, new ComponentByNameChooser('okButton')).clickMouse()
 
-        ULCPopupMenuOperator popUpMenu = tree.callPopupOnPath(pathForRename)
-        assertNotNull popUpMenu
-        popUpMenu.pushMenu("Start workflow")
+        //V2 added to the tree.
+        itemTree.selectRow(3)
+        int row = itemTree.getRowForPath(itemTree.findPath(['Application', 'Parameterization', 'Connection failed - contact support. vR2'] as String[]))
+        assert applicationModelRow + 3 == row
+        popupMenu = itemTree.callPopupOnCell(applicationModelRow + 3, 0)
+        ULCMenuItemOperator delete = new ULCMenuItemOperator(popupMenu, "Delete")
+        delete.clickMouse()
+        ULCDialogOperator deleteConfirm = new ULCDialogOperator(new ComponentByNameChooser('AlertDialog'))
+        ULCButtonOperator okButton = new ULCButtonOperator(deleteConfirm, new ComponentByNameChooser('AlertDialog.ok'))
+        okButton.clickMouse()
+        sleep(2000)
 
-        TreePath newPath = tree.findPath(["Application", "Parameterization", "Workflow", "ApplicationParameters vR1"] as String[])
-        assertNotNull "path not found", newPath
-
-        popUpMenu = tree.callPopupOnPath(newPath)
-        assertNotNull popUpMenu
-        popUpMenu.pushMenu("Send to reviewer")
-
-        popUpMenu = tree.callPopupOnPath(newPath)
-        popUpMenu.pushMenu("Reject")
-
-        newPath = tree.findPath(["Application", "Parameterization", "Workflow", "ApplicationParameters vR2"] as String[])
-        assertNotNull "path not found", newPath
-
-        popUpMenu = tree.callPopupOnPath(newPath)
-        assertNotNull popUpMenu
-        popUpMenu.pushMenu("Send to reviewer")
-
-        popUpMenu = tree.callPopupOnPath(newPath)
-        popUpMenu.pushMenu("In production")
-
+        //V2 removed from the tree.
+        row = itemTree.getRowForPath(itemTree.findPath(['Application', 'Parameterization', 'Connection failed - contact support. vR1'] as String[]))
+        assert applicationModelRow + 3 == row
     }
 
 }
