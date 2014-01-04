647ab8ff678ae14c8ff87a8677c3edcfa64a9719
fazl <fazl.rahman@art-allianz.com>
2014-01-04 22:02:22 +0100

    Add a little type safety

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/view/CompareSimulationMenuItem.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/view/CompareSimulationMenuItem.groovy
@@ -3,14 +3,15 @@ package org.pillarone.riskanalytics.application.ui.main.view
 import com.ulcjava.base.application.ULCMenuItem
 import com.ulcjava.base.application.event.ITreeSelectionListener
 import com.ulcjava.base.application.event.TreeSelectionEvent
+import org.pillarone.riskanalytics.application.ui.main.action.CompareSimulationsAction
 
 /**
  * @author fouad.jaada@intuitive-collaboration.com
  */
 class CompareSimulationMenuItem extends ULCMenuItem implements ITreeSelectionListener {
-    def compareSimulationsAction
+    CompareSimulationsAction compareSimulationsAction
 
-    def CompareSimulationMenuItem(compareSimulationsAction) {
+    def CompareSimulationMenuItem(CompareSimulationsAction compareSimulationsAction) {
         super(compareSimulationsAction);
         this.compareSimulationsAction = compareSimulationsAction;
     }
