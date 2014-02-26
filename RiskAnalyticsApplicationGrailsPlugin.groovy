73e0218001b1955b1a3353ad72c5745bf64e04a2
fazl <fazl.rahman@art-allianz.com>
2014-02-26 11:09:13 +0100

    Plugin v1.8.39

--- RiskAnalyticsApplicationGrailsPlugin.groovy
+++ RiskAnalyticsApplicationGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 
 class RiskAnalyticsApplicationGrailsPlugin {
     // the plugin version
-    def version = "1.8.38"
+    def version = "1.8.39"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
