eb13197883ea787275902480b7056016814dd99f
Matthias Ansorge <matthias.ansorge@canoo.com>
2014-03-05 16:08:25 +0100

    minor

--- test/unit/org/pillarone/riskanalytics/application/ui/simulation/queue/SimulationQueuePaneTests.groovy
+++ test/unit/org/pillarone/riskanalytics/application/ui/simulation/queue/SimulationQueuePaneTests.groovy
@@ -5,9 +5,6 @@ import org.pillarone.riskanalytics.application.ui.AbstractP1RATTestCase
 import org.pillarone.riskanalytics.application.ui.simulation.model.impl.queue.SimulationQueuePaneModel
 import org.pillarone.riskanalytics.application.ui.simulation.view.impl.queue.SimulationQueuePane
 
-/**
- * Created by matthiasa on 04.03.14.
- */
 class SimulationQueuePaneTests extends AbstractP1RATTestCase {
     @Override
     ULCComponent createContentPane() {
