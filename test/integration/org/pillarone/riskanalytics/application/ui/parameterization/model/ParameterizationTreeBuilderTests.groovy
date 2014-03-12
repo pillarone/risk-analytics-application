b657e1e69fa92ad661ee40dc35c9b5dcd8d43b91
Matthias Ansorge <matthias.ansorge@canoo.com>
2014-03-12 13:32:18 +0100

    changed order of tests

--- test/integration/org/pillarone/riskanalytics/application/ui/parameterization/model/ParameterizationTreeBuilderTests.groovy
+++ test/integration/org/pillarone/riskanalytics/application/ui/parameterization/model/ParameterizationTreeBuilderTests.groovy
@@ -25,55 +25,12 @@ class ParameterizationTreeBuilderTests {
 
     @Before
     void setUp() {
-        LocaleResources.setTestMode(true)
+        LocaleResources.testMode = true
     }
 
     @After
     void tearDown() {
-        LocaleResources.setTestMode(false)
-    }
-
-    @Test
-    void testTreeStructure() {
-
-        Parameterization parameterization
-        ModelStructure structure
-        Model model
-
-        new ParameterizationImportService().compareFilesAndWriteToDB(['Application'])
-        new ModelStructureImportService().compareFilesAndWriteToDB(['Application'])
-
-        model = new ApplicationModel()
-        model.init()
-
-        parameterization = ModellingItemFactory.getParameterization(ParameterizationDAO.findByName('ApplicationParameters'))
-        structure = ModellingItemFactory.getModelStructure(ModelStructureDAO.findByName('ApplicationStructure'))
-        parameterization.load()
-        structure.load()
-
-        ParameterizationTreeBuilder builder = new ParameterizationTreeBuilder(model, structure, parameterization)
-        def root = builder.root
-        assertNotNull root
-
-        assertEquals 5, root.childCount
-        def structureNode = root.getChildAt(0)
-        assertEquals ComponentUtils.getNormalizedName("hierarchyLevel"), structureNode.displayName
-
-        assertEquals 1, structureNode.childCount
-        def componentNode = structureNode.getChildAt(0)
-        assertEquals 1, componentNode.childCount
-
-        def parameterNode = componentNode.getChildAt(0)
-        assertEquals ComponentUtils.getNormalizedName("parmValue"), parameterNode.displayName
-        assertTrue parameterNode instanceof SimpleValueParameterizationTableTreeNode
-
-        def popNode = root.getChildAt(2).getChildAt(2)
-        assertEquals ComponentUtils.getNormalizedName("parmNestedMdp"), popNode.displayName
-        assertTrue popNode instanceof ParameterObjectParameterTableTreeNode
-        assertEquals 2, popNode.childCount
-
-        assertTrue popNode.getChildAt(0) instanceof ParameterizationClassifierTableTreeNode
-        assertTrue popNode.getChildAt(1) instanceof MultiDimensionalParameterizationTableTreeNode
+        LocaleResources.testMode = false
     }
 
     @Test
@@ -152,5 +109,48 @@ class ParameterizationTreeBuilderTests {
 
         assertEquals 4, subComponentNode.childCount
         assertEquals "Nested", subComponentNode.displayName
+    }
+
+    @Test
+    void testTreeStructure() {
+
+        Parameterization parameterization
+        ModelStructure structure
+        Model model
+
+        new ParameterizationImportService().compareFilesAndWriteToDB(['Application'])
+        new ModelStructureImportService().compareFilesAndWriteToDB(['Application'])
+
+        model = new ApplicationModel()
+        model.init()
+
+        parameterization = ModellingItemFactory.getParameterization(ParameterizationDAO.findByName('ApplicationParameters'))
+        structure = ModellingItemFactory.getModelStructure(ModelStructureDAO.findByName('ApplicationStructure'))
+        parameterization.load()
+        structure.load()
+
+        ParameterizationTreeBuilder builder = new ParameterizationTreeBuilder(model, structure, parameterization)
+        def root = builder.root
+        assertNotNull root
+
+        assertEquals 5, root.childCount
+        def structureNode = root.getChildAt(0)
+        assertEquals ComponentUtils.getNormalizedName("hierarchyLevel"), structureNode.displayName
+
+        assertEquals 1, structureNode.childCount
+        def componentNode = structureNode.getChildAt(0)
+        assertEquals 1, componentNode.childCount
+
+        def parameterNode = componentNode.getChildAt(0)
+        assertEquals ComponentUtils.getNormalizedName("parmValue"), parameterNode.displayName
+        assertTrue parameterNode instanceof SimpleValueParameterizationTableTreeNode
+
+        def popNode = root.getChildAt(2).getChildAt(2)
+        assertEquals ComponentUtils.getNormalizedName("parmNestedMdp"), popNode.displayName
+        assertTrue popNode instanceof ParameterObjectParameterTableTreeNode
+        assertEquals 2, popNode.childCount
+
+        assertTrue popNode.getChildAt(0) instanceof ParameterizationClassifierTableTreeNode
+        assertTrue popNode.getChildAt(1) instanceof MultiDimensionalParameterizationTableTreeNode
     }
 }
