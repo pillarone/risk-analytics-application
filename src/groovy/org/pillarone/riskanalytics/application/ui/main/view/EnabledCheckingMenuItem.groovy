518fc72618b6150a930db20cd28d26d40351f60d
fazl <fazl.rahman@art-allianz.com>
2014-04-26 22:02:35 +0200

    PMO-2764 Forgot to register menu as tree selection listener.

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/view/EnabledCheckingMenuItem.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/view/EnabledCheckingMenuItem.groovy
@@ -14,6 +14,10 @@ class EnabledCheckingMenuItem extends ULCMenuItem implements ITreeSelectionListe
     EnabledCheckingMenuItem(SelectionTreeAction action) {
         super(action)
         this.myAction = action
+        // Tell the tree to inform this menuitem when selections change.
+        // I hope it only tells us when the menu is actually popped up.
+        // I would hate to think that all the menus on all the nodes get informed whenever any selection changes.
+        action.tree.addTreeSelectionListener(this)
     }
 
     // This must be the code that changes the menu item's enablement dynamically
