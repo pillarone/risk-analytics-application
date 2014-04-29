4a7aef41edfce4199bcf52a848123023d1f68c2b
fazl <fazl.rahman@art-allianz.com>
2014-04-29 15:13:25 +0200

    ART-1196 Only allow delete REJECTED model if it is latest version in workflow.

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/view/item/ParameterizationUIItem.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/view/item/ParameterizationUIItem.groovy
@@ -3,6 +3,8 @@ package org.pillarone.riskanalytics.application.ui.main.view.item
 import com.ulcjava.base.application.util.ULCIcon
 import grails.util.Holders
 import groovy.transform.CompileStatic
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 import org.pillarone.riskanalytics.application.ui.base.model.modellingitem.NavigationTableTreeModel
 import org.pillarone.riskanalytics.application.ui.comment.view.NewCommentView
 import org.pillarone.riskanalytics.application.ui.main.view.IDetailView
@@ -26,6 +28,8 @@ import static org.pillarone.riskanalytics.core.workflow.Status.REJECTED
  * @author fouad.jaada@intuitive-collaboration.com
  */
 class ParameterizationUIItem extends ModellingUiItemWithModel {
+
+    protected static final Log LOG = LogFactory.getLog(ParameterizationUIItem.class)
 
     ParameterizationUIItem(Model model, Parameterization parameterization) {
         super(model, parameterization)
@@ -119,11 +123,21 @@ class ParameterizationUIItem extends ModellingUiItemWithModel {
 
     @Override
     boolean isDeletable() {
-        Parameterization parameterization = item as Parameterization
-        return parameterization.status == NONE ||
-               parameterization.status == DATA_ENTRY ||
-               parameterization.status == REJECTED
+        Parameterization p14n = item as Parameterization
 
+        // We only allow deleting REJECTED items that are the latest version in their tree
+        // So we don't leave a hole in the version sequence
+        //
+        if( p14n.status == REJECTED ){
+            SortedSet allVersions = new TreeSet(VersionNumber.getExistingVersions( p14n ))
+            if( p14n.versionNumber != allVersions.last() ) {
+                LOG.info( "NOT DELETING ${p14n.getNameAndVersion()} as later version exists: ${allVersions.last()}"   )
+                return false
+            }
+        }
+        return p14n.status == NONE ||
+               p14n.status == DATA_ENTRY ||
+               p14n.status == REJECTED
     }
 
     @Override
