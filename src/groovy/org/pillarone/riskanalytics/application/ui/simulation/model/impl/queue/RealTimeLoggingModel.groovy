cc24d2ff6a94575e5de801b7c6068819e21e318c
fazl <fazl.rahman@art-allianz.com>
2014-05-24 02:26:49 +0200

    Reduce log spam; demote 2 INFOs to DEBUGs

--- src/groovy/org/pillarone/riskanalytics/application/ui/simulation/model/impl/queue/RealTimeLoggingModel.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/simulation/model/impl/queue/RealTimeLoggingModel.groovy
@@ -116,10 +116,12 @@ public class RealTimeLoggingModel {
         }
     }
 
+    // Reduce technical noise in logs - demote to debug
+    //
     private class AddAppenderListener extends SimulationRuntimeInfoAdapter {
         @Override
         void starting(SimulationRuntimeInfo info) {
-            log.info('added appender')
+            log.debug('added appender')
             LoggingAppender.instance.loggingManager.addAppender(appender)
         }
 
@@ -127,7 +129,7 @@ public class RealTimeLoggingModel {
         @Override
         void finished(SimulationRuntimeInfo info) {
             LoggingAppender.instance.loggingManager.removeAppender(appender)
-            log.info('removed appender')
+            log.debug('removed appender')
         }
     }
 }
