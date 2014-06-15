572cfc6b13d464c20eceeea6919fc390d93a6f63
fazl <fazl.rahman@art-allianz.com>
2014-06-15 17:16:33 +0200

    PMO-2676 & PMO-2814 Avoid trying to transfer very large CSV files that have been known to require server reboot (out of heap -> kills gridnodes)

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/action/CsvExportAction.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/action/CsvExportAction.groovy
@@ -7,76 +7,86 @@ import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
 import org.pillarone.riskanalytics.application.ui.util.UIUtils
 import org.pillarone.riskanalytics.core.dataaccess.ResultAccessor
+import org.pillarone.riskanalytics.core.output.SimulationRun
 import org.pillarone.riskanalytics.core.output.SingleValueResult
 import org.pillarone.riskanalytics.core.simulation.item.Simulation
 /**
  * @author fouad.jaada@intuitive-collaboration.com
  */
 class CsvExportAction extends ExportItemAction {
-    Log LOG = LogFactory.getLog(CsvExportAction)
+    static Log LOG = LogFactory.getLog(CsvExportAction)
+    static final int MB = 1024 * 1024; // one megabyte
+    static final long MAX_CSV_TRANSFER_SIZE = 200 * MB;
 
     public CsvExportAction(ULCTableTree tree) {
         super(tree, "CsvExportAction", 'csv')
     }
 
     protected void exportItem(Simulation item, int itemCount, filePaths, ULCWindow ancestor) {
-        // Quick way to have a transaction ? (SingleValueResult table is empty.)
-        //
-        SingleValueResult.withTransaction { trx ->
-            String selectedFile = itemCount > 1 ? "${filePaths[0]}/${getFileName(item)}" : filePaths[0]
 
-            IFileStoreHandler fileStoreHandler = [
-                prepareFile: { OutputStream stream ->
-                    try {
-                        item.load()
-                        def simulationRun = item.simulationRun
-                        String fileName = ResultAccessor.exportCsv(simulationRun) //Was major bottleneck (writing CSV on serverside)
-                        if (fileName) {
-                            long t = System.currentTimeMillis()
-                            stream.write( UIUtils.toCSV(getSimulationSettings(simulationRun)).getBytes() )
-                            stream.flush()
-                            LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote settings to clientside CSV for Sim '${simulationRun.name}'");
+        try{
+            SimulationRun simulationRun = item.simulationRun
+            String fileName = ResultAccessor.exportCsv(simulationRun) //Was major bottleneck (writing CSV on serverside)
+            if (fileName) {
+                long fileSize = new File(fileName).size()
+                if( fileSize <= MAX_CSV_TRANSFER_SIZE ){
 
-                            FileInputStream fis = new FileInputStream(fileName)
-                            stream.write("\nITERATION,PERIOD,PATH,FIELD,VALUE,COLLECTOR,DATE\n".bytes)
-                            // PMO-2676 ? Probably heap exceed here.  If so, could loop and do transfer in chunks.
-                            //
-                            LOG.info("About to stream ${fileName} to clientside")
-                            t = System.currentTimeMillis()
-                            final SIZE = 1024 * 1024;
-                            byte[] chunk = new byte[SIZE];
-                            int nRead = -1;
-                            long nWrote = 0;
-                            while ( (nRead=fis.read( chunk, 0, SIZE )) != -1 ) {
-                                stream.write( chunk, 0, nRead );
-                                nWrote += nRead
-//                              LOG.info("Wrote chunk of $nRead bytes to clientside stream")
-                            }
-                            chunk = null // give gc chance asap
-                            stream.flush()
-                            LOG.info("Timed ${System.currentTimeMillis() - t} ms: streaming ${nWrote}-bytes of iters for sim: '${simulationRun.name}'");
-                        } else {
-                            showAlert("exportError")
-                        }
-                    } catch (Throwable t) {
-                        LOG.error("Export failed: " + t.message, t)
-                        showAlert("exportError")
-                    } finally {
-                        stream.close()
+                    SingleValueResult.withTransaction { trx ->
+                        String selectedFile = itemCount > 1 ? "${filePaths[0]}/${getFileName(item)}" : filePaths[0]
+
+                        IFileStoreHandler fileStoreHandler = [
+                                prepareFile: { OutputStream stream ->
+                                    try {
+                                        FileInputStream fis = new FileInputStream(fileName)
+
+                                        long t = System.currentTimeMillis()
+                                        stream.write( UIUtils.toCSV(getSimulationSettings(simulationRun, '\t')).getBytes() )
+                                        stream.flush()
+                                        LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote settings to clientside CSV for Sim '${simulationRun.name}'");
+
+                                        stream.write("\nITERATION,PERIOD,PATH,FIELD,VALUE,COLLECTOR,DATE\n".bytes)
+                                        // PMO-2676 ? Probably heap exceed here.  If so, could loop and do transfer in chunks.
+                                        //
+                                        LOG.info("About to stream ${fileName} to clientside")
+                                        t = System.currentTimeMillis()
+                                        final int CHUNK_SIZE = 1024 * 1024;
+                                        byte[] chunk = new byte[CHUNK_SIZE];
+                                        int nRead = -1;
+                                        long nWrote = 0;
+                                        while ( (nRead=fis.read( chunk, 0, CHUNK_SIZE )) != -1 ) {
+                                            stream.write( chunk, 0, nRead );
+                                            nWrote += nRead
+                                        }
+                                        chunk = null // give gc chance asap
+                                        stream.flush()
+                                        LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote ${nWrote}-bytes iter data to clientside CSV for Sim: '${simulationRun.name}'");
+                                    } finally {
+                                        stream.close() // should be safe even if ULC tries to close it again (InputStream.close() required to be idempotent)
+                                    }
+                                },
+                                onSuccess: { path, name ->
+                                    showInfoAlert("Successfully exported CSV file", "Filename: $name saved in folder:\n $path")
+                                },
+                                onFailure: { reason, description ->
+                                    LOG.error description
+                                    showAlert("exportError")
+                                }
+                        ] as IFileStoreHandler
+
+                        ClientContext.storeFile(fileStoreHandler, selectedFile, MAX_CSV_TRANSFER_SIZE, false)
                     }
-                },
-                onSuccess: { path, name ->
-                   // NOP
-                },
-                onFailure: { reason, description ->
-                    LOG.error description
-                    showAlert("exportError")
+                }else{
+                    showWarnAlert("File too large",
+                                  "Unfortunately, generated CSV file exceeds $MAX_CSV_TRANSFER_SIZE-byte limit ($fileSize bytes)\n\n"+
+                                  "Hint: The normal (Excel) Export is often smaller" )
                 }
-            ] as IFileStoreHandler
-
-            ClientContext.storeFile(fileStoreHandler, selectedFile, Long.MAX_VALUE, false)
+            } else {
+                showAlert("exportError")
+            }
+        }  catch (Throwable t) {
+            LOG.error("Export failed: " + t.message, t)
+            showAlert("exportError")
         }
-
     }
 
     // Maybe the validation in class ExportAction is for Excel's benefit ?
