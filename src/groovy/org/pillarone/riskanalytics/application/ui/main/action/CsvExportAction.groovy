b37c84ad68c9be9683d199d68c3e461c177bfad4
fazl <fazl.rahman@art-allianz.com>
2014-06-20 18:12:02 +0200

    PMO-2814 Quit early (building CSV) if rows exceed 1-meg too

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/action/CsvExportAction.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/action/CsvExportAction.groovy
@@ -24,64 +24,58 @@ class CsvExportAction extends ExportItemAction {
 
         try{
             SimulationRun simulationRun = item.simulationRun
-            String fileName = ResultAccessor.exportCsv(simulationRun) //Was major bottleneck (writing CSV on serverside)
+            String fileName = ResultAccessor.exportCsv(simulationRun, ONE_MEG, EXPORT_TRANSFER_MAX_BYTES) //Was major bottleneck (writing CSV on serverside)
             if (fileName) {
-                long fileSize = new File(fileName).size()
-                if( fileSize <= EXPORT_TRANSFER_MAX_BYTES ){
+                SingleValueResult.withTransaction { trx ->
+                    String selectedFile = itemCount > 1 ? "${filePaths[0]}/${getFileName(item)}" : filePaths[0]
 
-                    SingleValueResult.withTransaction { trx ->
-                        String selectedFile = itemCount > 1 ? "${filePaths[0]}/${getFileName(item)}" : filePaths[0]
+                    IFileStoreHandler fileStoreHandler = [
+                            prepareFile: { OutputStream stream ->
+                                try {
+                                    FileInputStream fis = new FileInputStream(fileName)
 
-                        IFileStoreHandler fileStoreHandler = [
-                                prepareFile: { OutputStream stream ->
-                                    try {
-                                        FileInputStream fis = new FileInputStream(fileName)
-
-                                        long t = System.currentTimeMillis()
-                                        stream.write( UIUtils.toCSV(getSimulationSettings(simulationRun, '\t')).getBytes() )
-                                        stream.flush()
-                                        LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote settings to clientside CSV for Sim '${simulationRun.name}'");
-
-                                        stream.write("\nITERATION,PERIOD,PATH,FIELD,VALUE,COLLECTOR,DATE\n".bytes)
-                                        // PMO-2676 ? Probably heap exceed here.  If so, could loop and do transfer in chunks.
-                                        //
-                                        LOG.info("About to stream ${fileName} to clientside")
-                                        t = System.currentTimeMillis()
-                                        final int CHUNK_SIZE = 1024 * 1024;
-                                        byte[] chunk = new byte[CHUNK_SIZE];
-                                        int nRead = -1;
-                                        long nWrote = 0;
-                                        while ( (nRead=fis.read( chunk, 0, CHUNK_SIZE )) != -1 ) {
-                                            stream.write( chunk, 0, nRead );
-                                            nWrote += nRead
-                                        }
-                                        chunk = null // give gc chance asap
-                                        stream.flush()
-                                        LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote ${nWrote}-bytes iter data to clientside CSV for Sim: '${simulationRun.name}'");
-                                    } finally {
-                                        stream.close() // should be safe even if ULC tries to close it again (InputStream.close() required to be idempotent)
+                                    stream.write( UIUtils.toCSV(getSimulationSettings(simulationRun, '\t')).getBytes() )
+                                    stream.write("\nITERATION,PERIOD,PATH,FIELD,VALUE,COLLECTOR,DATE\n".bytes)
+                                    LOG.info("About to stream ${fileName} to clientside")
+                                    long t = System.currentTimeMillis()
+                                    final int CHUNK_SIZE = ONE_MEG;
+                                    byte[] chunk = new byte[CHUNK_SIZE];
+                                    int nRead = -1;
+                                    long nWrote = 0;
+                                    while ( (nRead=fis.read( chunk, 0, CHUNK_SIZE )) != -1 ) {
+                                        stream.write( chunk, 0, nRead );
+                                        nWrote += nRead
                                     }
-                                },
-                                onSuccess: { path, name ->
-                                    showInfoAlert("Successfully exported CSV file", "Filename: $name saved in folder:\n $path")
-                                },
-                                onFailure: { reason, description ->
-                                    LOG.error description
-                                    showAlert("exportError")
+                                    chunk = null // give gc chance asap
+                                    stream.flush()
+                                    LOG.info("Timed ${System.currentTimeMillis() - t} ms: Wrote ${nWrote}-bytes iter data to clientside CSV for Sim: '${simulationRun.name}'");
+                                } finally {
+                                    stream.close() // should be safe even if ULC tries to close it again (InputStream.close() required to be idempotent)
                                 }
-                        ] as IFileStoreHandler
+                            },
+                            onSuccess: { path, name ->
+                                showInfoAlert("Successfully exported CSV file", "Filename: $name saved in folder:\n $path", true)
+                            },
+                            onFailure: { reason, description ->
+                                LOG.error description
+                                showAlert("exportError")
+                            }
+                    ] as IFileStoreHandler
 
-                        ClientContext.storeFile(fileStoreHandler, selectedFile, EXPORT_TRANSFER_MAX_BYTES, false)
-                    }
-                }else{
-                    showWarnAlert("File too large",
-                                  "Unfortunately, generated CSV file exceeds $EXPORT_TRANSFER_MAX_BYTES-byte limit ($fileSize bytes)\n\n"+
-                                  "Hint: The normal (Excel) Export is often smaller" )
+                    ClientContext.storeFile(fileStoreHandler, selectedFile, EXPORT_TRANSFER_MAX_BYTES, false)
                 }
             } else {
                 showAlert("exportError")
             }
-        }  catch (Throwable t) {
+        } catch( IllegalStateException e){
+            if(e.message?.startsWith("CSV ")){
+                showInfoAlert("CSV file exceeds limits", "Sorry, " + e.message)
+            } else {
+                LOG.error("Export failed: " + t.message, t)
+                showAlert("exportError")
+            }
+        }
+        catch (Throwable t) {
             LOG.error("Export failed: " + t.message, t)
             showAlert("exportError")
         }
