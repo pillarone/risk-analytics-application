bce130e2a45a639003e6e33058a4d1f136d684c8
fazl <fazl.rahman@art-allianz.com>
2014-06-21 23:12:16 +0200

    PMO-2825 Avoid log spam caused by inheriting from SelectionTreeAction

--- src/groovy/org/pillarone/riskanalytics/application/ui/main/action/ShowPropertiesAction.groovy
+++ src/groovy/org/pillarone/riskanalytics/application/ui/main/action/ShowPropertiesAction.groovy
@@ -1,17 +1,35 @@
 package org.pillarone.riskanalytics.application.ui.main.action
+
+import com.ulcjava.base.application.ULCAlert
 import com.ulcjava.base.application.ULCTableTree
+import com.ulcjava.base.application.UlcUtilities
 import com.ulcjava.base.application.event.ActionEvent
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.pillarone.riskanalytics.application.ui.base.action.ResourceBasedAction
+import org.pillarone.riskanalytics.application.ui.util.UIUtils
+
 /**
  * @author fazl.rahman@art-allianz.com
  */
-class ShowPropertiesAction extends SelectionTreeAction {
+class ShowPropertiesAction extends ResourceBasedAction {
+    private static Log LOG = LogFactory.getLog(ShowPropertiesAction)
 
+    ULCTableTree tree
 
     ShowPropertiesAction(ULCTableTree tree) {
-        super("ShowPropertiesAction", tree);
+        super("ShowPropertiesAction");
+        this.tree = tree
     }
 
     void doActionPerformed(ActionEvent event) {
-        showInfoAlert("Selection properties", "Selected ${getSelectedUIItems()?.size()} items", true)
+        int count = SelectionTreeAction.getSelectedUIItems(tree)?.size()
+        String msg = "${count} items selected"
+        LOG.info(msg)
+        UIUtils.showAlert( UlcUtilities.getWindowAncestor(tree),
+                msg, "Have a nice day..",
+                ULCAlert.INFORMATION_MESSAGE)
     }
+
+
 }
