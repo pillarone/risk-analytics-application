90aea9ea5a6266d332795c113ea229e8ad07f1a1
fazl <fazl.rahman@art-allianz.com>
2014-10-28 12:06:14 +0100

    Try activating existing splash screen facility

--- src/java/org/pillarone/riskanalytics/application/environment/applet/P1RATAppletLauncher.java
+++ src/java/org/pillarone/riskanalytics/application/environment/applet/P1RATAppletLauncher.java
@@ -5,9 +5,15 @@ import com.ulcjava.base.trusted.AllPermissionsBrowserService;
 import com.ulcjava.environment.applet.client.DefaultAppletLauncher;
 import org.pillarone.riskanalytics.application.client.AllPermissionsFileExtendedService;
 import org.pillarone.riskanalytics.application.environment.shared.UIManagerHelper;
+import org.pillarone.riskanalytics.application.ui.util.SplashScreen;
+import org.pillarone.riskanalytics.application.ui.util.SplashScreenHandler;
 
 public class P1RATAppletLauncher extends DefaultAppletLauncher {
     public void init() {
+        SplashScreenHandler splashScreenHandler = new SplashScreenHandler(new SplashScreen());
+        ClientEnvironmentAdapter.setMessageService(splashScreenHandler);
+        splashScreenHandler.showSplashScreen();
+
         super.init();
 
         UIManagerHelper.setLookAndFeel();
